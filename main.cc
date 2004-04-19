fe0c61a895acc34bfe3279c2c037dfbede82ffdf
helly <helly@642ea486-5414-0410-9d7f-a0204ed87703>
2004-04-19 21:14:46 +0000

    Add -- style options.

--- main.cc
+++ main.cc
@@ -12,6 +12,7 @@
 #include "globals.h"
 #include "parser.h"
 #include "dfa.h"
+#include "mbo_getopt.h"
 
 char *fileName;
 bool sFlag = false;
@@ -20,59 +21,83 @@ unsigned int oline = 1;
 
 using namespace std;
 
-int main(unsigned argc, char *argv[])
+static char *opt_arg = NULL;
+static int opt_ind = 1;
+
+static const mbo_opt_struct OPTIONS[] = {
+	{'?', 0, "help"},
+	{'b', 0, "bit-vectors"},
+	{'e', 0, "ecb"},
+	{'h', 0, "help"},
+	{'s', 0, "nested-ifs"},
+	{'v', 0, "version"}
+};
+
+int main(int argc, char *argv[])
 {
+	int c;
     fileName = NULL;
+    outputFileName = NULL;
+
     if (argc == 1) {
 		goto usage;
 	}
-    while(--argc > 1) {
-		char *p = *++argv;
-		if (*p != '-' || *(p+1) == '\0') {
+
+	while ((c = mbo_getopt(argc, argv, OPTIONS, &opt_arg, &opt_ind, 0))!=-1) {
+		switch (c) {
+	    case '?':
+			goto usage;
+	    case 'b':
+			sFlag = true;
+			bFlag = true;
+			break;
+	    case 'e':
+			xlat = asc2ebc;
+			talx = ebc2asc;
+			break;
+	    case 'h':
+			goto usage;
+		case 'f':
+			outputFileName = opt_arg;
+			break;
+	    case 's':
+			sFlag = true;
+			break;
+		case 'v':
+			goto version;
+		default:
 			goto usage;
 		}
-		while (*++p != '\0') {
-		    switch(*p){
-		    case 'e':
-				xlat = asc2ebc;
-				talx = ebc2asc;
-				break;
-		    case 's':
-				sFlag = true;
-				break;
-		    case 'b':
-				sFlag = true;
-				bFlag = true;
-				break;
-			case '-':
-				if (!strcmp(p, "-version")) {
-					goto version;
-				}
-				goto usage;
-		    default:
-				goto usage;
-		    }
-		}
-    }
-    fileName = *++argv;
-    if (!strcmp(fileName, "--version")) {
-    	goto version;
-    }
+	}
+
+	if (argc == opt_ind + 1)
+	{
+		fileName = argv[opt_ind];
+	}
+	else
+	{
+		goto usage;
+	}
+
     int fd;
-    if (fileName[0] == '-' && fileName[1] == '\0') {
+    if (fileName[0] == '-' && fileName[1] == '\0')
+    {
 		fileName = "<stdin>";
 		fd = 0;
-    } else {
-		if ((fd = open(fileName, O_RDONLY)) < 0) {
+    }
+    else
+    {
+		if ((fd = open(fileName, O_RDONLY)) < 0)
+		{
 		    cerr << "can't open " << fileName << "\n";
 		    return 1;
 		}
     }
-    parse(fd, cout);
+   	parse(fd, cout);
     return 0;
 
 usage:
-    cerr << "usage: re2c [-esb] name\n";
+    cerr << "usage: re2c [-esbvh] file\n";
     return 2;
 
 version:
