81818103d281491e4612476a73548cc38a4040f4
helly <helly@642ea486-5414-0410-9d7f-a0204ed87703>
2008-11-28 11:48:36 +0000

    - Remove temp files on success

--- re2c/bug2102138-test.sh
+++ re2c/bug2102138-test.sh
@@ -6,5 +6,10 @@ for n in $(find test -name '*2102138*.re'|sed 's,.re,,'); do
 	test -f ${n}.c.temp && mv -f ${n}.c.temp ${n}.c
 	gcc ${n}.c -o ${n}.o
 	./${n}.o >${n}.out
-	diff -du ${n}.txt ${n}.out >${n}.cc.diff && echo "OK" || echo "FAIL"
+	if $(diff -du ${n}.txt ${n}.out >${n}.out.diff); then
+	  echo "OK"
+	  rm -f ${n}.out ${n}.out.diff
+	else
+	  echo "FAIL"
+	fi
 done
