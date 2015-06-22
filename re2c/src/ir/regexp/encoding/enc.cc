skvadrik/re2c

commit f56196d29f6c29b37e3e95a6777714c237e1c71c

Simplified implementation of range union and difference.

re2c/src/ir/regexp/encoding/enc.cc
@@ -141,7 +141,7 @@ Range * Enc::encodeRange(uint32_t l, uint32_t h) const
 			for (uint32_t c = l + 1; c <= h; ++c)
 			{
 				const uint32_t ec = asc2ebc[c & 0xFF];
-				r = doUnion(r, new Range(ec, ec + 1));
+				r = range_union (r, new Range(ec, ec + 1));
 			}
 			break;
 		}
@@ -161,8 +161,8 @@ Range * Enc::encodeRange(uint32_t l, uint32_t h) const
 					{
 						Range * surrs = new Range(SURR_MIN, SURR_MAX + 1);
 						Range * error = new Range(UNICODE_ERROR, UNICODE_ERROR + 1);
-						r = doDiff(r, surrs);
-						r = doUnion(r, error);
+						r = range_diff (r, surrs);
+						r = range_union (r, error);
 						break;
 					}
 					case POLICY_IGNORE:
@@ -190,7 +190,7 @@ Range * Enc::fullRange() const
 	if (policy != POLICY_IGNORE)
 	{
 		Range * surrs = new Range(SURR_MIN, SURR_MAX + 1);
-		r = doDiff(r, surrs);
+		r = range_diff (r, surrs);
 	}
 	return r;
 }

re2c/src/ir/regexp/regexp.cc
@@ -84,7 +84,7 @@ MatchOp * merge (MatchOp * m1, MatchOp * m2)
 	{
 		return m1;
 	}
-	MatchOp * m = new MatchOp (doUnion (m1->match, m2->match));
+	MatchOp * m = new MatchOp (range_union (m1->match, m2->match));
 	if (m1->ins_access == RegExp::PRIVATE
 		|| m2->ins_access == RegExp::PRIVATE)
 	{
@@ -114,7 +114,7 @@ RegExp * mkDiff (RegExp * e1, RegExp * e2)
 	{
 		return NULL;
 	}
-	Range * r = doDiff (m1->match, m2->match);
+	Range * r = range_diff (m1->match, m2->match);
 	return r
 		? (RegExp *) new MatchOp(r)
 		: (RegExp *) new NullOp;
@@ -216,7 +216,7 @@ Range * Scanner::mkRange(SubStr &s) const
 {
 	Range *r = getRange(s);
 	while (s.len > 0)
-		r = doUnion(r, getRange(s));
+		r = range_union (r, getRange(s));
 
 	return r;
 }
@@ -251,7 +251,7 @@ RegExp * Scanner::invToRE (SubStr & s) const
 
 	Range * r = s.len == 0
 		? full
-		: doDiff(full, mkRange (s));
+		: range_diff (full, mkRange (s));
 
 	return matchSymbolRange(r);
 }
@@ -263,7 +263,7 @@ RegExp * Scanner::mkDot() const
 	if (!encoding.encode(c))
 		fatalf("Bad code point: '0x%X'", c);
 	Range * ran = new Range(c, c + 1);
-	Range * inv = doDiff(full, ran);
+	Range * inv = range_diff (full, ran);
 
 	return matchSymbolRange(inv);
 }

re2c/src/util/range.cc
@@ -3,117 +3,69 @@
 namespace re2c
 {
 
-Range *doUnion(Range *r1, Range *r2)
+Range * range_union (Range * r1, Range * r2)
 {
-	if (r1 == NULL)
-		return r2;
-	if (r2 == NULL)
-		return r1;
-
-	Range *r, **rP = &r;
-
+	Range * r = NULL;
+	Range ** p = &r;
 	for (;;)
 	{
-		Range *s;
-
-		if (r1->lb <= r2->lb)
+		if (!r1)
 		{
-			s = new Range(r1->lb, r1->ub);
+			* p = r2;
+			break;
 		}
-		else
+		if (!r2)
 		{
-			s = new Range(r2->lb, r2->ub);
+			* p = r1;
+			break;
 		}
-
-		*rP = s;
-		rP = &s->next;
-
-		for (;;)
+		if (r2->lb < r1->lb) // swap
 		{
-			if (r1->lb <= r2->lb)
+			Range * r1_old = r1;
+			r1 = r2;
+			r2 = r1_old;
+		}
+		uint32_t ub = r1->ub;
+		if (r2->lb < r1->ub)
+		{
+			for (; r2 && r2->lb < r1->ub; r2 = r2->next)
 			{
-				if (r1->lb > s->ub)
-					break;
-
-				if (r1->ub > s->ub)
-					s->ub = r1->ub;
-
-				if (!(r1 = r1->next))
+				if (r1->ub < r2->ub)
 				{
-					uint32_t ub = 0;
-
-					for (; r2 && r2->lb <= s->ub; r2 = r2->next)
-						ub = r2->ub;
-
-					if (ub > s->ub)
-						s->ub = ub;
-
-					*rP = r2;
-
-					return r;
-				}
-			}
-			else
-			{
-				if (r2->lb > s->ub)
-					break;
-
-				if (r2->ub > s->ub)
-					s->ub = r2->ub;
-
-				if (!(r2 = r2->next))
-				{
-					uint32_t ub = 0;
-
-					for (; r1 && r1->lb <= s->ub; r1 = r1->next)
-						ub = r1->ub;
-
-					if (ub > s->ub)
-						s->ub = ub;
-
-					*rP = r1;
-
-					return r;
+					ub = r2->ub;
 				}
 			}
 		}
+		* p = new Range (r1->lb, ub);
+		p = &(* p)->next;
+		r1 = r1->next;
 	}
-
-	*rP = NULL;
 	return r;
 }
 
-Range *doDiff(Range *r1, Range *r2)
+Range * range_diff (Range * r1, Range * r2)
 {
-	Range *r, *s, **rP = &r;
-
+	Range * r = NULL;
+	Range ** p = &r;
 	for (; r1; r1 = r1->next)
 	{
+		for (; r2 && r2->ub <= r1->lb; r2 = r2->next);
 		uint32_t lb = r1->lb;
-
-		for (; r2 && r2->ub <= r1->lb; r2 = r2->next)
-
-			;
 		for (; r2 && r2->lb < r1->ub; r2 = r2->next)
 		{
 			if (lb < r2->lb)
 			{
-				*rP = s = new Range(lb, r2->lb);
-				rP = &s->next;
+				* p = new Range(lb, r2->lb);
+				p = &(* p)->next;
 			}
-
-			if ((lb = r2->ub) >= r1->ub)
-				goto noMore;
+			lb = r2->ub;
 		}
-
-		*rP = s = new Range(lb, r1->ub);
-		rP = &s->next;
-
-noMore:
-		;
+		if (lb < r1->ub)
+		{
+			* p = new Range(lb, r1->ub);
+			p = &(* p)->next;
+		}
 	}
-
-	*rP = NULL;
 	return r;
 }
 

re2c/src/util/range.h
@@ -1,7 +1,7 @@
 #ifndef _RE2C_UTIL_RANGE_
 #define _RE2C_UTIL_RANGE_
 
-#include <iostream>
+#include <stddef.h> // NULL
 
 #include "src/util/c99_stdint.h"
 #include "src/util/forbid_copy.h"
@@ -10,31 +10,32 @@
 namespace re2c
 {
 
-class Range
+struct Range
 {
-
-public:
-	Range		*next;
-	uint32_t	lb, ub;	// [lb,ub)
-
 	static free_list<Range*> vFreeList;
 
-public:
-	Range(uint32_t l, uint32_t u) : next(NULL), lb(l), ub(u)
-	{
-		vFreeList.insert(this);
-	}
+	Range * next;
+	// [lb,ub)
+	uint32_t lb;
+	uint32_t ub;
 
-	~Range()
+	Range (uint32_t l, uint32_t u)
+		: next (NULL)
+		, lb (l)
+		, ub (u)
 	{
-		vFreeList.erase(this);
+		vFreeList.insert (this);
+	}
+	~Range ()
+	{
+		vFreeList.erase (this);
 	}
 
 	FORBID_COPY (Range);
 };
 
-Range *doUnion(Range *r1, Range *r2);
-Range *doDiff(Range *r1, Range *r2);
+Range * range_union (Range * r1, Range * r2);
+Range * range_diff (Range * r1, Range * r2);
 
 } // end namespace re2c
 

re2c/src/parse/scanner.cc
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>

commit 3e939ded3ff7788dfd1247f315cc3d70d2bb46b6

Operations on character classes: fixed subtraction, simplified addition.
Subtraction was broken by commit f56196d29f6c29b37e3e95a6777714c237e1c71c:
"Simplified implementation of range union and difference."

re2c/src/ir/regexp/encoding/enc.cc
@@ -132,16 +132,16 @@ Range * Enc::encodeRange(uint32_t l, uint32_t h) const
 		case ASCII:
 			l &= 0xFF;
 			h &= 0xFF;
-			r = new Range(l, h + 1);
+			r = Range::ran (l, h + 1);
 			break;
 		case EBCDIC:
 		{
 			const uint32_t el = asc2ebc[l & 0xFF];
-			r = new Range(el, el + 1);
+			r = Range::sym (el);
 			for (uint32_t c = l + 1; c <= h; ++c)
 			{
 				const uint32_t ec = asc2ebc[c & 0xFF];
-				r = range_union (r, new Range(ec, ec + 1));
+				r = Range::add (r, Range::sym (ec));
 			}
 			break;
 		}
@@ -149,7 +149,7 @@ Range * Enc::encodeRange(uint32_t l, uint32_t h) const
 		case UTF16:
 		case UTF32:
 		case UTF8:
-			r = new Range(l, h + 1);
+			r = Range::ran (l, h + 1);
 			if (l <= SURR_MAX && h >= SURR_MIN)
 			{
 				switch (policy)
@@ -159,10 +159,10 @@ Range * Enc::encodeRange(uint32_t l, uint32_t h) const
 						break;
 					case POLICY_SUBSTITUTE:
 					{
-						Range * surrs = new Range(SURR_MIN, SURR_MAX + 1);
-						Range * error = new Range(UNICODE_ERROR, UNICODE_ERROR + 1);
-						r = range_diff (r, surrs);
-						r = range_union (r, error);
+						Range * surrs = Range::ran (SURR_MIN, SURR_MAX + 1);
+						Range * error = Range::sym (UNICODE_ERROR);
+						r = Range::sub (r, surrs);
+						r = Range::add (r, error);
 						break;
 					}
 					case POLICY_IGNORE:
@@ -186,11 +186,11 @@ Range * Enc::encodeRange(uint32_t l, uint32_t h) const
  */
 Range * Enc::fullRange() const
 {
-	Range * r = new Range(0, nCodePoints());
+	Range * r = Range::ran (0, nCodePoints());
 	if (policy != POLICY_IGNORE)
 	{
-		Range * surrs = new Range(SURR_MIN, SURR_MAX + 1);
-		r = range_diff (r, surrs);
+		Range * surrs = Range::ran (SURR_MIN, SURR_MAX + 1);
+		r = Range::sub (r, surrs);
 	}
 	return r;
 }

re2c/src/ir/regexp/encoding/range_suffix.cc
@@ -27,7 +27,7 @@ RegExp * emit(RangeSuffix * p, RegExp * re)
 		RegExp * regexp = NULL;
 		for (; p != NULL; p = p->next)
 		{
-			RegExp * re1 = doCat(new MatchOp(new Range(p->l, p->h + 1)), re);
+			RegExp * re1 = doCat(new MatchOp(Range::ran (p->l, p->h + 1)), re);
 			regexp = doAlt(regexp, emit(p->child, re1));
 		}
 		return regexp;

re2c/src/ir/regexp/encoding/utf16/utf16_regexp.cc
@@ -9,12 +9,12 @@ namespace re2c {
 RegExp * UTF16Symbol(utf16::rune r)
 {
 	if (r <= utf16::MAX_1WORD_RUNE)
-		return new MatchOp(new Range(r, r + 1));
+		return new MatchOp(Range::sym (r));
 	else
 	{
 		const uint16_t ld = utf16::lead_surr(r);
 		const uint16_t tr = utf16::trail_surr(r);
-		return new CatOp(new MatchOp(new Range(ld, ld + 1)), new MatchOp(new Range(tr, tr + 1)));
+		return new CatOp(new MatchOp(Range::sym (ld)), new MatchOp(Range::sym (tr)));
 	}
 }
 

re2c/src/ir/regexp/encoding/utf8/utf8_regexp.cc
@@ -10,9 +10,9 @@ RegExp * UTF8Symbol(utf8::rune r)
 {
 	uint8_t chars[utf8::MAX_RUNE_LENGTH];
 	const int chars_count = utf8::rune_to_bytes(chars, r);
-	RegExp * re = new MatchOp(new Range(chars[0], chars[0] + 1));
+	RegExp * re = new MatchOp(Range::sym (chars[0]));
 	for (int i = 1; i < chars_count; ++i)
-		re = new CatOp(re, new MatchOp(new Range(chars[i], chars[i] + 1)));
+		re = new CatOp(re, new MatchOp(Range::sym (chars[i])));
 	return re;
 }
 

re2c/src/ir/regexp/regexp.cc
@@ -84,7 +84,7 @@ MatchOp * merge (MatchOp * m1, MatchOp * m2)
 	{
 		return m1;
 	}
-	MatchOp * m = new MatchOp (range_union (m1->match, m2->match));
+	MatchOp * m = new MatchOp (Range::add (m1->match, m2->match));
 	if (m1->ins_access == RegExp::PRIVATE
 		|| m2->ins_access == RegExp::PRIVATE)
 	{
@@ -143,7 +143,7 @@ RegExp * Scanner::matchSymbol(uint32_t c) const
 	else if (encoding.is(Enc::UTF8))
 		return UTF8Symbol(c);
 	else
-		return new MatchOp(new Range(c, c + 1));
+		return new MatchOp (Range::sym (c));
 }
 
 RegExp * Scanner::strToRE (SubStr & s) const
@@ -202,7 +202,7 @@ Range * Scanner::mkRange(SubStr &s) const
 {
 	Range *r = getRange(s);
 	while (s.len > 0)
-		r = range_union (r, getRange(s));
+		r = Range::add (r, getRange(s));
 
 	return r;
 }
@@ -252,7 +252,7 @@ RegExp * Scanner::invToRE (SubStr & s) const
 
 	Range * r = s.len == 0
 		? full
-		: range_diff (full, mkRange (s));
+		: Range::sub (full, mkRange (s));
 
 	return matchSymbolRange(r);
 }
@@ -265,7 +265,7 @@ RegExp * Scanner::mkDiff (RegExp * e1, RegExp * e2) const
 	{
 		fatal("can only difference char sets");
 	}
-	Range * r = range_diff (m1->match, m2->match);
+	Range * r = Range::sub (m1->match, m2->match);
 
 	return matchSymbolRange (r);
 }
@@ -276,8 +276,8 @@ RegExp * Scanner::mkDot() const
 	uint32_t c = '\n';
 	if (!encoding.encode(c))
 		fatalf("Bad code point: '0x%X'", c);
-	Range * ran = new Range(c, c + 1);
-	Range * inv = range_diff (full, ran);
+	Range * ran = Range::sym (c);
+	Range * inv = Range::sub (full, ran);
 
 	return matchSymbolRange(inv);
 }
@@ -294,7 +294,7 @@ RegExp * Scanner::mkDot() const
  */
 RegExp * Scanner::mkDefault() const
 {
-	Range * def = new Range(0, encoding.nCodeUnits());
+	Range * def = Range::ran (0, encoding.nCodeUnits());
 	return new MatchOp(def);
 }
 

re2c/src/main.cc
@@ -70,7 +70,6 @@ uint32_t last_fill_index = 0;
 CodeNames mapCodeName;
 
 free_list<RegExp*> RegExp::vFreeList;
-free_list<Range*>  Range::vFreeList;
 
 using namespace std;
 

re2c/src/util/range.cc
@@ -3,70 +3,95 @@
 namespace re2c
 {
 
-Range * range_union (Range * r1, Range * r2)
+free_list<Range*> Range::vFreeList;
+
+void Range::append_overlapping (Range * & head, Range * & tail, const Range * r)
 {
-	Range * r = NULL;
-	Range ** p = &r;
-	for (;;)
+	if (!head)
 	{
-		if (!r1)
-		{
-			* p = r2;
-			break;
-		}
-		if (!r2)
-		{
-			* p = r1;
-			break;
-		}
-		if (r2->lb < r1->lb) // swap
-		{
-			Range * r1_old = r1;
-			r1 = r2;
-			r2 = r1_old;
-		}
-		uint32_t ub = r1->ub;
-		if (r2->lb < r1->ub)
-		{
-			for (; r2 && r2->lb < r1->ub; r2 = r2->nx)
-			{
-				if (r1->ub < r2->ub)
-				{
-					ub = r2->ub;
-				}
-			}
-		}
-		* p = new Range (r1->lb, ub);
-		p = &(* p)->nx;
-		r1 = r1->nx;
+		head = Range::ran (r->lb, r->ub);
+		tail = head;
 	}
-	return r;
+	else if (tail->ub < r->lb)
+	{
+		tail->nx = Range::ran (r->lb, r->ub);
+		tail = tail->nx;
+	}
+	else if (tail->ub < r->ub)
+	{
+		tail->ub = r->ub;
+	}
 }
 
-Range * range_diff (Range * r1, Range * r2)
+Range * Range::add (const Range * r1, const Range * r2)
 {
-	Range * r = NULL;
-	Range ** p = &r;
+	Range * head = NULL;
+	Range * tail = NULL;
+	for (; r1 && r2;)
+	{
+		if (r1->lb < r2->lb)
+		{
+			append_overlapping (head, tail, r1);
+			r1 = r1->nx;
+		}
+		else
+		{
+			append_overlapping (head, tail, r2);
+			r2 = r2->nx;
+		}
+	}
 	for (; r1; r1 = r1->nx)
 	{
-		for (; r2 && r2->ub <= r1->lb; r2 = r2->nx);
-		uint32_t lb = r1->lb;
-		for (; r2 && r2->lb < r1->ub; r2 = r2->nx)
-		{
-			if (lb < r2->lb)
-			{
-				* p = new Range(lb, r2->lb);
-				p = &(* p)->nx;
-			}
-			lb = r2->ub;
-		}
-		if (lb < r1->ub)
-		{
-			* p = new Range(lb, r1->ub);
-			p = &(* p)->nx;
-		}
+		append_overlapping (head, tail, r1);
 	}
-	return r;
+	for (; r2; r2 = r2->nx)
+	{
+		append_overlapping (head, tail, r2);
+	}
+	return head;
 }
 
-} // end namespace re2c
+void Range::append (Range ** & ptail, uint32_t l, uint32_t u)
+{
+	Range * & tail = * ptail;
+	tail = Range::ran (l, u);
+	ptail = &tail->nx;
+}
+
+Range * Range::sub (const Range * r1, const Range * r2)
+{
+	Range * head = NULL;
+	Range ** ptail = &head;
+	while (r1)
+	{
+		if (!r2 || r2->lb >= r1->ub)
+		{
+			append (ptail, r1->lb, r1->ub);
+			r1 = r1->nx;
+		}
+		else if (r2->ub <= r1->lb)
+		{
+			r2 = r2->nx;
+		}
+		else
+		{
+			if (r1->lb < r2->lb)
+			{
+				append (ptail, r1->lb, r2->lb);
+			}
+			while (r2 && r2->ub < r1->ub)
+			{
+				const uint32_t lb = r2->ub;
+				r2 = r2->nx;
+				const uint32_t ub = r2 && r2->lb < r1->ub
+					? r2->lb
+					: r1->ub;
+				append (ptail, lb, ub);
+			}
+			r1 = r1->nx;
+		}
+	}
+	return head;
+}
+
+} // namespace re2c

re2c/src/util/range.h
@@ -23,13 +23,13 @@ private:
 	uint32_t ub;
 
 public:
-	Range (uint32_t l, uint32_t u)
-		: nx (NULL)
-		, lb (l)
-		, ub (u)
+	static Range * sym (uint32_t c)
 	{
-		assert (lb < ub);
-		vFreeList.insert (this);
+		return new Range (NULL, c, c + 1);
+	}
+	static Range * ran (uint32_t l, uint32_t u)
+	{
+		return new Range (NULL, l, u);
 	}
 	~Range ()
 	{
@@ -38,12 +38,24 @@ public:
 	Range * next () const { return nx; }
 	uint32_t lower () const { return lb; }
 	uint32_t upper () const { return ub; }
-	friend Range * range_union (Range * r1, Range * r2);
-	friend Range * range_diff (Range * r1, Range * r2);
+	static Range * add (const Range * r1, const Range * r2);
+	static Range * sub (const Range * r1, const Range * r2);
+
+private:
+	Range (Range * n, uint32_t l, uint32_t u)
+		: nx (n)
+		, lb (l)
+		, ub (u)
+	{
+		assert (lb < ub);
+		vFreeList.insert (this);
+	}
+	static void append_overlapping (Range * & head, Range * & tail, const Range * r);
+	static void append (Range ** & ptail, uint32_t l, uint32_t u);
 
 	FORBID_COPY (Range);
 };
 
-} // end namespace re2c
+} // namespace re2c
 
 #endif // _RE2C_UTIL_RANGE_