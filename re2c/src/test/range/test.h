8aa4db21bdbb83e5c4b47dbdb26842417cf69748
Ulya Trofimovich <skvadrik@gmail.com>
2015-06-23 22:02:27 +0100

    Comment on test for character class operations.

--- re2c/src/test/range/test.h
+++ re2c/src/test/range/test.h
@@ -7,6 +7,16 @@ namespace re2c { class Range; }
 
 namespace re2c_test {
 
+/*
+ * If encoding has N code units (characters), character class can be
+ * represented as an N-bit integer: k-th bit is set iff k-th character
+ * belongs to the class.
+ *
+ * Addition and subtraction can be implemented trivially for such
+ * integer representation of character classes: addition is simply
+ * bitwise OR of two classes, subtraction is bitwise AND of the first
+ * class and negated second class.
+ */
 template <uint8_t BITS> re2c::Range * range (uint32_t n);
 template <uint8_t BITS> re2c::Range * add (uint32_t n1, uint32_t n2);
 template <uint8_t BITS> re2c::Range * sub (uint32_t n1, uint32_t n2);
