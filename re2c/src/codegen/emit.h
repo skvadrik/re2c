27a83c0ffd2c86eb9ddbfc855c73e5305bcdc15a
Ulya Trofimovich <skvadrik@gmail.com>
2015-09-27 12:03:03 +0100

    Don't hang forever trying to replace empty configuration arguments.

--- re2c/src/codegen/emit.h
+++ re2c/src/codegen/emit.h
@@ -25,12 +25,15 @@ void genGoTo (OutputFile & o, uint32_t ind, const State * from, const State * to
 
 template<typename _Ty> std::string replaceParam (std::string str, const std::string & param, const _Ty & value)
 {
-	std::ostringstream strValue;
-	strValue << value;
-	std::string::size_type pos;
-	while((pos = str.find(param)) != std::string::npos)
+	if (!param.empty ())
 	{
-		str.replace(pos, param.length(), strValue.str());
+		std::ostringstream strValue;
+		strValue << value;
+		std::string::size_type pos;
+		while((pos = str.find(param)) != std::string::npos)
+		{
+			str.replace(pos, param.length(), strValue.str());
+		}
 	}
 	return str;
 }
