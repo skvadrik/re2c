2d566806c4fce03f9e996594616a9706a43c0783
Ulya Trofimovich <skvadrik@gmail.com>
2015-10-20 12:47:56 +0100

    Manual.

--- gen.sh
+++ gen.sh
@@ -8,5 +8,6 @@ cp -R "$srcdir" "$objdir"
 
 for f in `find "$objdir" -type f -name "*.rst"`
 do
-    rst2html.py --link-stylesheet --stylesheet="css/default.css" "$f" > "${f%.rst}.html"
+    rst2html.py --stylesheet="$objdir/css/default.css" "$f" > "${f%.rst}.html"
+#    rst2html.py --link-stylesheet --stylesheet="css/default.css" "$f" > "${f%.rst}.html"
 done
