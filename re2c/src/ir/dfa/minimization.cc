skvadrik/re2c

commit 5958a2366cdb1361b5158076924014a36ff19dcc

Bind contexts (a.k.a. tags) to DFA transitions, not states.
This is a very imortant change: it makes tracing tag conflicts as
simple as comparing tags on transitions during DFA construction.
If during determinization we get two identical transitions that
differ only in tags, then we have a tag conflict. Tags that cause
conflicts are called non-deterministic (since they don't allow
deterministic match).

This approach if very similar to Ville Laurikari's TDFA: as he does,
we first build TNFA and then apply determinization; however Laurikari's
TDFA uses complex bookkeeping to track all possible tag values,
while re2c simply forbids tags that cannot be matche efficiently.

Binding tags to transitions allows more fine-grained liveness
analyses, dead tag elimination and tag deduplication.

re2c/src/ir/dfa/minimization.cc
@@ -45,8 +45,8 @@ static void minimization_table(
 		for (size_t j = 0; j < i; ++j)
 		{
 			dfa_state_t *s2 = states[j];
-			tbl[i][j] = s1->tags != s2->tags
-				|| s1->rule != s2->rule;
+			tbl[i][j] = s1->rule != s2->rule
+				|| s1->rule_tags != s2->rule_tags;
 		}
 	}
 
@@ -68,9 +68,10 @@ static void minimization_table(
 							std::swap(oi, oj);
 						}
 						if (oi != oj &&
-							(oi == dfa_t::NIL ||
-							oj == dfa_t::NIL ||
-							tbl[oi][oj]))
+							(oi == dfa_t::NIL
+							|| oj == dfa_t::NIL
+							|| tbl[oi][oj]
+							|| memcmp(states[i]->tags, states[j]->tags, nchars * sizeof(size_t)) != 0))
 						{
 							tbl[i][j] = true;
 							loop = true;
@@ -137,7 +138,7 @@ static void minimization_moore(
 	for (size_t i = 0; i < count; ++i)
 	{
 		dfa_state_t *s = states[i];
-		std::pair<size_t, size_t> key(s->rule, s->tags);
+		std::pair<size_t, size_t> key(s->rule, s->rule_tags);
 		if (init.insert(std::make_pair(key, i)).second)
 		{
 			part[i] = i;
@@ -186,7 +187,8 @@ static void minimization_moore(
 					size_t k = diff[n];
 					if (memcmp(&out[j * nchars],
 						&out[k * nchars],
-						nchars * sizeof(size_t)) == 0)
+						nchars * sizeof(size_t)) == 0
+						&& memcmp(states[j]->tags, states[k]->tags, nchars * sizeof(size_t)) == 0)
 					{
 						part[j] = k;
 						next[j] = next[k];

commit 71e94f3e4ebac528c7dd22e43121b8aac797cc02

Fixed tags comparison in table minimization.
Bug was introduced in commit 5958a2366cdb1361b5158076924014a36ff19dcc
"Bind contexts (a.k.a. tags) to DFA transitions, not states.":
instead of comparing tags of single transition on one particular
symbol, we compared tags for all outgoing transitions at once
(probably copy-paste from Moore minimization).

The bug was revealed by multiple tests (including '--skeleton' ones)
as soon as I ran testsute with '--dfa-determinization table'.

re2c/src/ir/dfa/minimization.cc
@@ -67,11 +67,11 @@ static void minimization_table(
 						{
 							std::swap(oi, oj);
 						}
-						if (oi != oj &&
-							(oi == dfa_t::NIL
-							|| oj == dfa_t::NIL
-							|| tbl[oi][oj]
-							|| memcmp(states[i]->tags, states[j]->tags, nchars * sizeof(size_t)) != 0))
+						if (states[i]->tags[k] != states[j]->tags[k]
+							|| (oi != oj
+								&& (oi == dfa_t::NIL
+								|| oj == dfa_t::NIL
+								|| tbl[oi][oj])))
 						{
 							tbl[i][j] = true;
 							loop = true;
@@ -188,8 +188,10 @@ static void minimization_moore(
 					if (memcmp(&out[j * nchars],
 						&out[k * nchars],
 						nchars * sizeof(size_t)) == 0
-						&& memcmp(states[j]->tags, states[k]->tags, nchars * sizeof(size_t)) == 0)
-					{
+					 && memcmp(states[j]->tags,
+						states[k]->tags,
+						nchars * sizeof(size_t)) == 0
+					 ) {
 						part[j] = k;
 						next[j] = next[k];
 						next[k] = j;