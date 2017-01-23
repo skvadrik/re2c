0c9681ee60d30c469863fa2b7c0a50ed42b65df4
Ulya Trofimovich <skvadrik@gmail.com>
2017-01-23 16:19:24 +0000

    Updated parser for bison-3.0.4.

--- re2c/bootstrap/src/parse/y.tab.h
+++ re2c/bootstrap/src/parse/y.tab.h
@@ -1,20 +1,19 @@
-/* A Bison parser, made by GNU Bison 2.4.3.  */
+/* A Bison parser, made by GNU Bison 3.0.4.  */
 
-/* Skeleton interface for Bison's Yacc-like parsers in C
-   
-      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006,
-   2009, 2010 Free Software Foundation, Inc.
-   
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
+
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
@@ -27,31 +26,39 @@
    special exception, which will cause the skeleton and the resulting
    Bison output files to be licensed under the GNU General Public
    License without this special exception.
-   
+
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
-
-/* Tokens.  */
-#ifndef YYTOKENTYPE
-# define YYTOKENTYPE
-   /* Put the tokens into the symbol table, so that GDB and other debuggers
-      know about them.  */
-   enum yytokentype {
-     TOKEN_CLOSESIZE = 258,
-     TOKEN_CODE = 259,
-     TOKEN_CONF = 260,
-     TOKEN_ID = 261,
-     TOKEN_FID = 262,
-     TOKEN_FID_END = 263,
-     TOKEN_REGEXP = 264
-   };
+#ifndef YY_YY_BOOTSTRAP_SRC_PARSE_Y_TAB_H_INCLUDED
+# define YY_YY_BOOTSTRAP_SRC_PARSE_Y_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
 #endif
 
+/* Token type.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    TOKEN_CLOSESIZE = 258,
+    TOKEN_CODE = 259,
+    TOKEN_CONF = 260,
+    TOKEN_ID = 261,
+    TOKEN_FID = 262,
+    TOKEN_FID_END = 263,
+    TOKEN_REGEXP = 264
+  };
+#endif
 
-
+/* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-typedef union YYSTYPE
+
+union YYSTYPE
 {
 
 
@@ -63,13 +70,16 @@ typedef union YYSTYPE
 	re2c::CondList * clist;
 
 
+};
 
-} YYSTYPE;
+typedef union YYSTYPE YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
-# define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
 #endif
 
+
 extern YYSTYPE yylval;
 
+int yyparse (re2c::context_t &context);
 
+#endif /* !YY_YY_BOOTSTRAP_SRC_PARSE_Y_TAB_H_INCLUDED  */
