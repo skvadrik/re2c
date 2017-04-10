b15338a79902f300d630dd79b63961a6895f2768
Ulya Trofimovich <skvadrik@gmail.com>
2017-04-10 13:41:17 +0100

    Autogenerate help from manpage.

---
+++ re2c/genhelp.sh
@@ -0,0 +1,11 @@
+
+echo "const char *help =" > "$2"
+PAGER=cat man "$1" \
+    | tail -n +7 \
+    | head -n -3 \
+    | sed -E -e 's/\x1b\x5b[0-9]+m//g' \
+    | sed 's/\\x/\\\\x/g' \
+    | sed -E 's/"/\\"/g' \
+    | sed -E 's/(.*)/"\1\\n"/' \
+    >> "$2"
+echo ";" >> "$2"
