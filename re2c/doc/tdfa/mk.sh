42a34612013c4705c57943788108cd80faa8bf38
Ulya Trofimovich <skvadrik@gmail.com>
2017-08-09 14:04:10 +0100

    Paper on Lookahead TDFA: added bibliography.

--- re2c/doc/tdfa/mk.sh
+++ re2c/doc/tdfa/mk.sh
@@ -1,3 +1,6 @@
 #!/bin/sh -e
 
 pdflatex -shell-escape tdfa.tex </dev/null > tdfa.build_log
+bibtex tdfa
+pdflatex -shell-escape tdfa.tex </dev/null > tdfa.build_log
+pdflatex -shell-escape tdfa.tex </dev/null > tdfa.build_log
