b98e70f6a845d3a0e5520ea6e59600b2f70714d4
Ulya Trofimovich <skvadrik@gmail.com>
2019-01-27 10:06:22 +0000

    Adjusted build system to correctly build DLLs for windows.
    Adjustments:
        - configure.ac:  pass win32-dll option to LT_INIT
    
        - Makefile_libre2c_posix.am: use -no-undefined in LDFLAGS
    
        - use slibtool: https://github.com/midipix-project/slibtool for windows
          builds The problem with libtool is that it doesn't allow to link libstdc++
          and libgcc statically, which is necessary to build portable DLLs with
          Mingw. Libtool adds -nostdlib option to LDFLAGS and links some predefined
          objects that pull in dependency on dynamic libstdc++ and libgcc, even in
          the presence of -static-libstdc++ -static-libgcc.

--- re2c/__build_mingw.sh
+++ re2c/__build_mingw.sh
@@ -5,9 +5,9 @@ rm -rf $builddir
 mkdir $builddir
 
 cd $builddir
-../configure \
+../configure LDFLAGS="-static-libstdc++ -static-libgcc" \
     --enable-debug \
     --enable-libs \
     --host i686-w64-mingw32 \
-    && make -j5
+    && make -j5 LIBTOOL="dlibtool"
 cd ..
