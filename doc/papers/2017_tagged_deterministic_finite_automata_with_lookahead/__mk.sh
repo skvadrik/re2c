b093c9f86b9eb27592b9f82b1e197f09b2e96fb2
Ulya Trofimovich <skvadrik@gmail.com>
2019-07-26 10:31:20 +0100

    Paper: cleaned up source files (mostly removed unused files).
    Touched the following papers:
    - 1994 paper "RE2C: a More Versatile Parser Generator"
    - 2017 paper "Tagged Deterministic Finite Automata with Lookahead" (after arXiv publication)
    - 2019 paper "Efficient POSIX Submatch Extraction on NFA"
    
    All .pdf and .ps files have been moved to gh-pages-gen branch.

---
+++ doc/papers/2017_tagged_deterministic_finite_automata_with_lookahead/__mk.sh
@@ -0,0 +1,3 @@
+#!/bin/sh -e
+
+pdflatex -shell-escape 2017_trofimovich_tagged_deterministic_finite_automata_with_lookahead.tex </dev/null > tdfa.build_log
--- doc/tdfa/mk.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -e
-
-pdflatex -shell-escape tdfa.tex </dev/null > tdfa.build_log
-bibtex tdfa
-pdflatex -shell-escape tdfa.tex </dev/null > tdfa.build_log
-pdflatex -shell-escape tdfa.tex </dev/null > tdfa.build_log
