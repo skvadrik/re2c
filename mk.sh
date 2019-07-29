74e1eacf148148b0f4d7bff69ed5b8c7aeaebdca
Ulya Trofimovich <skvadrik@gmail.com>
2019-07-29 16:12:53 +0100

    Updated build script to reflect the changes in directory layout on master branch.

--- mk.sh
+++ mk.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/sh -x
 
 # sync with master (pull manpage files)
 remote=`git config branch.master.remote` \
     && git fetch $remote \
-    && { git archive --remote=. remotes/$remote/master re2c/doc/manual \
-        | tar -C src/ --strip-components=2 -xpf -; } \
-    && { git archive --remote=. remotes/$remote/master re2c/examples/*.re \
-        | tar -C src/ --strip-components=1 -xpf -; }
+    && { git archive --remote=. remotes/$remote/master doc/manual \
+        | tar -C src/ --strip-components=1 -xpf -; } \
+    && { git archive --remote=. remotes/$remote/master examples/*.re \
+        | tar -C src/ --strip-components=0 -xpf -; }
 
 sphinx-build -b html src obj
 
