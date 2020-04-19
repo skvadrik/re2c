e3e43bcbb746dd6692f2d60ed1fa2e26c8cbe987
Ulya Trofimovich <skvadrik@gmail.com>
2020-04-19 10:38:21 +0100

    Rewrite recursion into iteration (skeleton max path length computation).
    Ensured that the new algorithm computes the same length as the old one
    by adding temporary debug print of the computed length to both
    algorithms and verifying that it coincides.
    
    This is to avoid stack overflow on large RE (especially on instrumented
    builds that have larger stack frames, like AddressSanitizer).
    
    Stack overflow reported by Agostino Sarubbo.

--- src/skeleton/maxpath.cc
+++ src/skeleton/maxpath.cc
@@ -13,70 +13,101 @@
 
 
 namespace re2c {
+namespace {
 
 // 0 < DIST_MAX < DIST_ERROR <= std::numeric_limits<uint32_t>::max()
 static const uint32_t DIST_ERROR = std::numeric_limits<uint32_t>::max();
 static const uint32_t DIST_MAX = DIST_ERROR - 1;
 
-// maximal distance to end node (assuming one iteration per loop)
-// different from YYMAXFILL calculation
-// in the way it handles loops and empty regexp
-static uint32_t calc_dist(const Skeleton &skel
-    , std::vector<uint8_t> &loops
-    , std::vector<uint32_t> &dists
-    , size_t i)
-{
-    const Node &node = skel.nodes[i];
-    uint32_t dist = dists[i];
+struct StackItem {
+    uint32_t node;
+    uint32_t dist;
+    Node::arcs_t::const_iterator arc;
+};
 
-    if (dist != DIST_ERROR) {
-        return dist;
-    }
+} // anonymous namespace
 
-    else if (node.end()) {
-        return dists[i] = 0;
-    }
-
-    // we cut the looping path, so the current node is like
-    // the "end" node; but the actual value for this node
-    // is yet to be calculated on the recursive return
-    else if (loops[i] > 1) {
-        return 0;
-    }
-
-    // unroll one iteration of loops
-    // (must be consistent with skeleton data generation)
-    else {
-        local_inc _(loops[i]);
-
-        Node::arcs_t::const_iterator
-            arc = node.arcs.begin(),
-            end = node.arcs.end();
-
-        // handle all child states before setting this state's suffix
-        for (; arc != end; ++arc) {
-            const uint32_t d = calc_dist(skel, loops, dists, arc->first);
-
-            // not necessarily true for dists[arc->first]
-            DASSERT (d != DIST_ERROR);
-
-            dist = (dist == DIST_ERROR) ? d : std::max(dist, d);
-        }
-
-        return dists[i] = std::min(dist + 1, DIST_MAX);
-    }
-}
-
-// calculate maximal path length, check overflow
+// Calculate maximal path length (check overflow).
+// Maximal distance to end node (assuming one iteration per loop) is different
+// from YYMAXFILL calculation in the way it handles loops and empty regexp.
 uint32_t maxpath(const Skeleton &skel)
 {
     std::vector<uint8_t> loops(skel.nodes_count);
     std::vector<uint32_t> dists(skel.nodes_count, DIST_ERROR);
-    const uint32_t maxlen = calc_dist(skel, loops, dists, 0);
-    if (maxlen == DIST_MAX) {
+    std::vector<StackItem> stack;
+    stack.reserve(skel.nodes_count);
+
+    // DFS "return value"
+    uint32_t dist = 0;
+
+    const StackItem i0 = {0, 0, skel.nodes[0].arcs.begin()};
+    stack.push_back(i0);
+
+    while (!stack.empty()) {
+        StackItem i = stack.back();
+        stack.pop_back();
+        const Node &node = skel.nodes[i.node];
+
+        if (i.arc == node.arcs.begin()) {
+            // DFS recursive enter
+            if (dists[i.node] != DIST_ERROR) {
+                // already computed distance for this node
+            }
+            else if (node.end() || loops[i.node] > 1) {
+                // terminate recursion, set zero distance (loops are unrolled
+                // once, which must be in sync with skeleton data generation)
+                dists[i.node] = 0;
+            }
+            else {
+                ++loops[i.node];
+                DASSERT(i.dist == 0);
+
+                const uint32_t succ = static_cast<uint32_t>(i.arc->first);
+
+                // reschedule this node with the next successor
+                StackItem k = {i.node, 0, ++i.arc};
+                stack.push_back(k);
+
+                // schedule the first successor node
+                StackItem j = {succ, 0, skel.nodes[succ].arcs.begin()};
+                stack.push_back(j);
+            }
+            dist = dists[i.node];
+        }
+        else if (i.arc == node.arcs.end()) {
+            // DFS recursive return
+            --loops[i.node];
+
+            // use last successor's distance
+            DASSERT(dist != DIST_ERROR);
+            dist = std::max(i.dist, dist);
+
+            // all successors traversed, set this node's distance
+            DASSERT(dist < DIST_MAX);
+            dist = dists[i.node] = dist + 1;
+            if (dist == DIST_MAX) break;
+        }
+        else {
+            // use he previous successor's distance
+            DASSERT(dist != DIST_ERROR);
+            dist = std::max(i.dist, dist);
+
+            const uint32_t succ = static_cast<uint32_t>(i.arc->first);
+
+            // reschedule this node with the next successor and updated distance
+            StackItem k = {i.node, dist, ++i.arc};
+            stack.push_back(k);
+
+            // schedule the current successor node
+            StackItem j = {succ, 0, skel.nodes[succ].arcs.begin()};
+            stack.push_back(j);
+        }
+    }
+
+    if (dist == DIST_MAX) {
         fatal("DFA path %sis too long", incond(skel.cond).c_str());
     }
-    return maxlen;
+    return dist;
 }
 
 } // namespace re2c
