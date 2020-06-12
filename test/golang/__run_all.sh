e87f231052b6cf40d62c95999070ecf6ff70c87f
Ulya Trofimovich <skvadrik@gmail.com>
2020-06-12 22:42:56 +0100

    Add a script to run Go test programs.

---
+++ test/golang/__run_all.sh
@@ -0,0 +1,21 @@
+
+root_dir=$(pwd)
+
+for f in $(find -name '*.re'); do
+    echo $f
+    cd $(dirname $f)
+
+    cat $(basename ${f%.re}.go) \
+        | egrep -v 'warning: rule .*matches empty string \[-Wmatch-empty-string\]' \
+        | egrep -v 'warning: tag .* degree of nondeterminism \[-Wnondeterministic-tags\]' \
+        > x.go
+
+    test -z "$(grep 're2c: error:' x.go)" || continue
+
+    go run x.go >/dev/null || { echo "*** error ***"; exit 1; }
+
+    rm -f x.go
+    cd $root_dir
+done
+
+echo "All good."
