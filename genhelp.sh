1ab43f38b1d790c2d2296a5682a259f8db510c2d
Ulya Trofimovich <skvadrik@gmail.com>
2020-06-28 10:54:21 +0100

    Fix the way help message is generated, update its bootstrap file.
    Changes:
    - Use MANPAGER instead of PAGER (no significant difference).
    - Set MANWIDTH=100 and to get deterministic line width.
    - Tweak genhelp.sh as it was cutting off too many lines in the generated
      output (presumably something has changed in the way man works).

--- genhelp.sh
+++ genhelp.sh
@@ -2,9 +2,9 @@
 > "$2"
 echo "extern const char *help;" >> "$2"
 echo "const char *help =" >> "$2"
-PAGER=cat man "$1" \
-    | tail -n +7 \
-    | head -n -3 \
+MANPAGER=cat MANWIDTH=100 man "$1" \
+    | tail -n +6 \
+    | head -n -1 \
     | sed -E -e 's/\x1b\x5b[0-9]+m//g' \
     | sed 's/\\x/\\\\x/g' \
     | sed -E 's/"/\\"/g' \
