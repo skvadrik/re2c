b4f65a723e61a942ad732d2e6056d5c0bfcee4b0
Ulya Trofimovich <skvadrik@gmail.com>
2020-06-29 21:16:50 +0100

    Use pregenerated output for running `gotest` on Go examples.

--- examples/go/__run_all.sh
+++ examples/go/__run_all.sh
@@ -1,23 +1,15 @@
-
-root_dir=$(pwd)
-export RE2C=$root_dir/../../__build/re2c
-
 for f in $(find -name '*.re'); do
     echo $f
-
-    gofile="example.go"
+    gofile="${f%.re}.go"
     gotest="example_test.go"
-    cp "$f" "$gofile"
-    go generate "$gofile"
 
-    cat "$gotest" \
+    cat "$gofile" \
         | egrep -v 'warning: rule .*matches empty string \[-Wmatch-empty-string\]' \
         | egrep -v 'warning: tag .* degree of nondeterminism \[-Wnondeterministic-tags\]' \
-        > "$gotest.1"
-    mv "$gotest.1" "$gotest"
+        > "$gotest"
 
     go test "$gotest" || { echo "*** error ***"; exit 1; }
-    rm -f "$gofile" "$gotest"
+    rm -f "$gotest"
 done
 
 echo "All good."
