9311e5295cac5ac76456ad78160fe0bd2f3ba2f0
Ulya Trofimovich <skvadrik@gmail.com>
2020-07-29 18:28:52 +0100

    Fix release script to update re2c version for CMake.

--- release.sh
+++ release.sh
@@ -7,12 +7,12 @@ echo 'RUNME: cppcheck --enable=all --inconclusive --std=posix --quiet --force -I
 echo "RUNME: uselex `find <objdir> -type f -name '*.o'`"
 echo 'RUNME: configure CFLAGS="-ffunction-sections -fdata-sections" LDFLAGS="-Wl,--gc-sections -Wl,--print-gc-sections"'
 
-if [[ $# -ne 1 ]]
-then
-    echo "usage: ./release.sh <version>"
+if [[ $# -ne 2 ]]; then
+    echo "usage: ./release.sh <version> <builddir>"
     exit 1
 fi
 version="$1"
+builddir="$2"
 
 branch=`git rev-parse --abbrev-ref HEAD`
 if [[ $branch != "master" ]]
@@ -21,16 +21,20 @@ then
     exit 1
 fi
 
-# edit version in configure.in
-lcontext="AC_INIT\(\[re2c\],\["
-rcontext="\],\[re2c-general@lists\.sourceforge\.net\]\)"
+# update version
 old="[0-9]+(\.[0-9]+)*(\.dev)?"
 new=$version
+# edit version in configure.ac
+lcontext="AC_INIT\(\[re2c\],\["
+rcontext="\],\[re2c-general@lists\.sourceforge\.net\]\)"
 sed -i -E "s/$lcontext$old$rcontext/$lcontext$new$rcontext/" configure.ac
+# edit version in CMakeLists.txt
+lcontext="project\(re2c VERSION "
+rcontext=" "
+sed -i -E "s/$lcontext$old$rcontext/$lcontext$new$rcontext/" CMakeLists.txt
 
 # distcheck
-builddir=.build
-cd re2c && ./__distcheck.sh $builddir && cd ..
+build/__distcheck.sh "$builddir"
 
 # commit release
 git commit -a -m "Release $version."
