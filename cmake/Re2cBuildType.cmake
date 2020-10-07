22014a13808429de714bbba20366cda6e4123f6c
Serghei Iakovlev <egrep@protonmail.ch>
2020-10-07 11:51:33 +0300

    Don't keep ALLOWABLE_BUILD_TYPES variable in global scope

--- cmake/Re2cBuildType.cmake
+++ cmake/Re2cBuildType.cmake
@@ -1,9 +1,11 @@
-set(ALLOWABLE_BUILD_TYPES Debug Release RelWithDebInfo MinSizeRel)
+set(_allowable_build_types Debug Release RelWithDebInfo MinSizeRel)
 
 # Specify the build type
 if(NOT CMAKE_BUILD_TYPE)
     message(STATUS "Setting build type to 'Release' as none was specified.")
     set(CMAKE_BUILD_TYPE Debug CACHE STRING "Choose the type of build." FORCE)
-elseif(NOT CMAKE_BUILD_TYPE IN_LIST ALLOWABLE_BUILD_TYPES)
+elseif(NOT CMAKE_BUILD_TYPE IN_LIST _allowable_build_types)
     message(FATAL_ERROR "Invalid build type: ${CMAKE_BUILD_TYPE}")
 endif()
+
+unset(_allowable_build_types)
