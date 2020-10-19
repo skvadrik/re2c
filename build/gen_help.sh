9c203791a534a87f9060b30a5d3bf8657dc1a5ea
Serghei Iakovlev <egrep@protonmail.ch>
2020-10-19 22:48:17 +0300

    Add the ability to generate manpage and documentation for BSD systems
    - Stripping headers may not work as expected, as man output differs
      on different systems. This should work now.
    - Simplified output parsing procedure by replacing the regular
      expression by TERM=dumb.
    - Stripping escape sequences.
    - Added documentation about the intentions of this script.
    
    For more see: https://github.com/skvadrik/re2c/issues/325

--- build/gen_help.sh
+++ build/gen_help.sh
@@ -1,13 +1,29 @@
+#!/usr/bin/env sh
 
-> "$2"
-echo "extern const char *help;" >> "$2"
-echo "const char *help =" >> "$2"
-MANPAGER=cat MANWIDTH=100 man "$1" \
-    | tail -n +6 \
-    | head -n -1 \
-    | sed -E -e 's/\x1b\x5b[0-9]+m//g' \
-    | sed 's/\\x/\\\\x/g' \
-    | sed -E 's/"/\\"/g' \
-    | sed -E 's/(.*)/"\1\\n"/' \
-    >> "$2"
-echo ";" >> "$2"
+{
+    # We can safely export the following variables unless we source this file
+    export TERM=dumb
+    export MANPAGER=cat
+    export MANWIDTH=80
+
+    echo "extern const char *help;"
+    echo "const char *help ="
+
+    # Here is how it works:
+    #
+    # 1. 'col -b' removes backspaces, 'col -x' replaces tabs with spaces
+    # 2. Drop lines from the top up to USAGE word
+    # 3. Drop two lines from the bottom
+    # 4. Escape \x by \\x
+    # 5. Escape " by \"
+    # 6. Wrap each line in double quotes " and adding a newline character \n
+    man "$1"                        \
+        | col -bx                   \
+        | grep -A 1000 USAGE        \
+        | sed '$d' | sed '$d'       \
+        | sed 's/\\x/\\\\x/g'       \
+        | sed -E 's/"/\\"/g'        \
+        | sed -E 's/(.*)/"\1\\n"/'
+
+    echo ";"
+} > "$2"
