3eab74d9fd1ff47323f2c485e4da7cc3cb01ecaa
Ulya Trofimovich <skvadrik@gmail.com>
2020-10-30 07:51:56 +0000

    Provide default initialize and copy operations for wrap iterator.
    Depending on the STL implementation these operations are required for
    vectors of wrap iterators (in particular, push_back() method may require
    a default assignment operator).

--- src/util/wrap_iter.h
+++ src/util/wrap_iter.h
@@ -11,14 +11,41 @@ class wrap_citer_t
     typedef typename container_t::const_iterator citer_t;
     typedef const typename container_t::value_type* cpval_t;
 
-    const citer_t beg;
-    const citer_t end;
+    citer_t beg;
+    citer_t end;
     citer_t cur;
 
 public:
-    explicit wrap_citer_t(const container_t &c): beg(c.begin()), end(c.end()), cur(beg) {}
-    wrap_citer_t& operator++() { if (++cur == end) cur = beg; return *this; }
-    cpval_t operator->() const { return cur.operator->(); }
+    wrap_citer_t(): beg(), end(), cur() {}
+
+    explicit wrap_citer_t(const container_t &c)
+        : beg(c.begin())
+        , end(c.end())
+        , cur(beg)
+    {}
+
+    wrap_citer_t(const wrap_citer_t &w)
+        : beg(w.beg)
+        , end(w.end)
+        , cur(w.cur)
+    {}
+
+    wrap_citer_t& operator=(const wrap_citer_t &w)
+    {
+        new (this) wrap_citer_t<container_t>(w);
+        return *this;
+    }
+
+    wrap_citer_t& operator++()
+    {
+        if (++cur == end) cur = beg;
+        return *this;
+    }
+
+    cpval_t operator->() const
+    {
+        return cur.operator->();
+    }
 };
 
 } // namespace re2c
