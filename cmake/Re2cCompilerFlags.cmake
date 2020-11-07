3d6789449c54e0f7330a33456f23c9393f326cd8
Serghei Iakovlev <egrep@protonmail.ch>
2020-11-07 13:40:45 +0200

    Speed up the CMake configuration by avoiding unnecessary checks

--- cmake/Re2cCompilerFlags.cmake
+++ cmake/Re2cCompilerFlags.cmake
@@ -6,7 +6,7 @@ include(CheckCXXCompilerFlag)
 # which makes it hard to test for them with autoconf.)
 function(try_cxxflag flag)
     set(varname "cxxflag_${flag}")
-    string(REPLACE "=" "_" varname "${varname}")
+    string(MAKE_C_IDENTIFIER "${varname}" varname)
     check_cxx_compiler_flag("${flag}" ${varname})
     if(${varname})
         set(CMAKE_CXX_FLAGS "${flag} ${CMAKE_CXX_FLAGS}")
@@ -17,35 +17,46 @@ function(try_cxxflag flag)
     endif()
 endfunction()
 
+get_property(isMultiConfig  GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
+
 # Set default optimization flag if there is no build type provided.
-# For more see discussion at https://github.com/skvadrik/re2c/pull/316
-if(NOT CMAKE_BUILD_TYPE)
+# CMAKE_BUILD_TYPE is not used by multi-configuration generators.
+if(NOT CMAKE_BUILD_TYPE AND NOT isMultiConfig)
     try_cxxflag("-O2")
 endif()
 
-try_cxxflag("-W")
-try_cxxflag("-Wall")
-try_cxxflag("-Wextra")
-try_cxxflag("-Weffc++")
-try_cxxflag("-pedantic")
-try_cxxflag("-Wformat=2")
-try_cxxflag("-Wredundant-decls")
-try_cxxflag("-Wsuggest-attribute=format")
-try_cxxflag("-Wconversion")
-try_cxxflag("-Wsign-conversion")
-try_cxxflag("-Werror=return-type")
-try_cxxflag("-Weverything"
-    "-Wno-unknown-warning-option" # CLANG eats some GCC options only to warn they are unknown
-    "-Wno-reserved-id-macro" # to allow header guards of the form '_RE2C_PATH_TO_HEADER_BASENAME_'
-    "-Wno-padded"
-    "-Wno-old-style-cast" # RE2C-generated lexer has lots of C-style casts because of 're2c:yych:conversion = 1;'
-    "-Wno-nested-anon-types"
-    "-Wno-global-constructors" # initialization of global constants with std::numeric_limits<...> (mostly for size_t)
-    "-Wno-shadow-field-in-constructor" # using same names in ctor seems more like a feature
-    "-Wno-undefined-func-template" # explicit specialization to reduce build dependencies
-)
+# The following flags are not supported by Visual Studio.
+# Thus, we can speed up the configuration of the project
+# by avoiding unnecessary checks.
+if (NOT CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+    try_cxxflag("-W")
+    try_cxxflag("-Wall")
+    try_cxxflag("-Wextra")
+    try_cxxflag("-Weffc++")
+    try_cxxflag("-pedantic")
+    try_cxxflag("-Wformat=2")
+    try_cxxflag("-Wredundant-decls")
+    try_cxxflag("-Wsuggest-attribute=format")
+    try_cxxflag("-Wconversion")
+    try_cxxflag("-Wsign-conversion")
+    try_cxxflag("-Werror=return-type")
+    try_cxxflag("-Weverything"
+      "-Wno-unknown-warning-option" # CLANG eats some GCC options only to warn they are unknown
+      "-Wno-reserved-id-macro" # to allow header guards of the form '_RE2C_PATH_TO_HEADER_BASENAME_'
+      "-Wno-padded"
+      "-Wno-old-style-cast" # RE2C-generated lexer has lots of C-style casts because of 're2c:yych:conversion = 1;'
+      "-Wno-nested-anon-types"
+      "-Wno-global-constructors" # initialization of global constants with std::numeric_limits<...> (mostly for size_t)
+      "-Wno-shadow-field-in-constructor" # using same names in ctor seems more like a feature
+      "-Wno-undefined-func-template" # explicit specialization to reduce build dependencies
+      )
 
-try_cxxflag("-fdiagnostics-color=always")
+    try_cxxflag("-fdiagnostics-color=always")
+else()
+    # /Wall enables all warnings that are off by default including
+    # some of the ones GCC/Clang enables through -Wxxxx flags.
+    try_cxxflag("/Wall")
+endif()
 
 # Print compiler and linker flags
 message(STATUS "C compiler flags: ${CMAKE_C_FLAGS}")
