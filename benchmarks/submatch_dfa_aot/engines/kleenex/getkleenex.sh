85f3bcaea21083ed07f81d4802064e0abb6c6195
Ulya Trofimovich <skvadrik@gmail.com>
2021-03-20 20:04:46 +0000

    Drop Kleenex patch, as the upstream has been fixed.

--- benchmarks/submatch_dfa_aot/engines/kleenex/getkleenex.sh
+++ benchmarks/submatch_dfa_aot/engines/kleenex/getkleenex.sh
@@ -5,7 +5,6 @@ git clone --recursive https://github.com/diku-kmc/kleenexlang.git
 cd kleenexlang
 cabal v1-sandbox init && cabal v1-sandbox add-source regexps-syntax
 cabal v1-install --dependencies-only
-git apply ../0001-Updating-for-new-GHC.patch
 cabal v1-configure && cabal v1-build
 cd ..
 rm -f kexc
