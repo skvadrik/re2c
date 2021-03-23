8139571a1642c0e36b0b99341564ed432e2bd82d
Ulya Trofimovich <skvadrik@gmail.com>
2021-03-23 23:49:40 +0000

    Remove similar cases in submatch DFA JIT benchmark.

--- benchmarks/submatch_dfa_jit/bench.cc
+++ benchmarks/submatch_dfa_jit/bench.cc
@@ -22,40 +22,30 @@ int main(int argc, char** argv)
     const char *aaa_strings[] = {aaa, NULL};
 
     const std::vector<bench_t> benches {
-        {"HTTP-RFC7230",   MESSAGE_HEAD,  http_strings, 0},
-        {"HTTP-simple",    MESSAGE_HEAD2, http_strings, 0},
-
-        {"URI-RFC3986",    URI,           uri_strings,  0},
-        {"URI-simple",     URI2,          uri_strings,  0},
-
-        {"IPv6",           IPV6,          ipv6_strings, 0},
-        {"IPv6-simple",    IPV62,         ipv6_strings, 0},
-        {"IPv4",           IPV4,          ipv4_strings, 0},
-        {"IPv4-simple",    IPV42,         ipv4_strings, 0},
+        {"http-rfc7230",   MESSAGE_HEAD,  http_strings, 0},
+        {"http-simple",    MESSAGE_HEAD2, http_strings, 0},
+        {"uri-rfc3986",    URI,           uri_strings,  0},
+        {"uri-simple",     URI2,          uri_strings,  0},
+        {"ipv6",           IPV6,          ipv6_strings, 0},
+        {"ipv6-simple",    IPV62,         ipv6_strings, 0},
+        {"ipv4",           IPV4,          ipv4_strings, 0},
+        {"ipv4-simple",    IPV42,         ipv4_strings, 0},
         {"date",           DATE,          date_strings, 0},
         {"date-simple",    DATE2,         date_strings, 0},
         {"package",        PACKAGE_ATOM,  atom_strings, 0},
         {"package-simple", PACKAGE_ATOM2, atom_strings, 0},
 
-        {NULL, "((a{2}|a{3}|a{5})*)a{1000}",             aaa_strings, 0},
-        {NULL, "((((a){2})|((a){3})|((a){5}))*)a{1000}", aaa_strings, 0},
-
-        {NULL, "((a|aa)*)a{1000}",     aaa_strings, 0},
-        {NULL, "(((a)|(aa))*)a{1000}", aaa_strings, 0},
-
-        {NULL, "((a?)*)a{1000}",   aaa_strings, 0},
-        {NULL, "(((a)?)*)a{1000}", aaa_strings, 0},
+        {NULL, "(a)*a{1}",    aaa_strings, 0},
+        {NULL, "(a)*a{10}",   aaa_strings, 0},
+        {NULL, "(a)*a{100}",  aaa_strings, 0},
+        {NULL, "(a)*a{1000}", aaa_strings, 0},
     };
 
     const std::vector<alg_t> algs {
-        {"TDFA-OS",           ENGINE_RE2C, 0},
-        {"TDFA-LG",           ENGINE_RE2C, REG_LEFTMOST},
-        {"regless-TDFA-OS",   ENGINE_RE2C, REG_REGLESS},
-        {"regless-TDFA-LG",   ENGINE_RE2C, REG_REGLESS | REG_LEFTMOST},
-        //{"staDFA-OS",         ENGINE_RE2C, REG_STADFA},
-#ifdef HAVE_RE2_RE2_H
-        //{"re2-LG",            ENGINE_RE2,  0},
-#endif
+        {"tdfa1-P",         ENGINE_RE2C, 0},
+        {"tdfa1-L",         ENGINE_RE2C, REG_LEFTMOST},
+        {"regless-tdfa1-P", ENGINE_RE2C, REG_REGLESS},
+        {"regless-tdfa1-L", ENGINE_RE2C, REG_REGLESS | REG_LEFTMOST},
     };
 
     benchmark::Initialize(&argc, argv);
@@ -65,7 +55,7 @@ int main(int argc, char** argv)
             if (alg.flags & bench.not_flags) continue;
 
             const char *name = bench.name ? bench.name : bench.regexp;
-            snprintf(title, MAX_TITLE, "%s-gen_%s", name, alg.name);
+            snprintf(title, MAX_TITLE, "%s-regcomp_%s", name, alg.name);
             benchmark::RegisterBenchmark(title, bench_regcomp_t(), alg, bench)
                 ->Unit(benchmark::kMicrosecond);
         }
@@ -73,7 +63,7 @@ int main(int argc, char** argv)
             if (alg.flags & bench.not_flags) continue;
 
             const char *name = bench.name ? bench.name : bench.regexp;
-            snprintf(title, MAX_TITLE, "%s-run_%s", name, alg.name);
+            snprintf(title, MAX_TITLE, "%s-regexec_%s", name, alg.name);
             benchmark::RegisterBenchmark(title, bench_regexec_t(), alg, bench)
                 ->Unit(benchmark::kMicrosecond);
         }
