6d7c3cbb47d55e6ec8b5d0876d667fd0b2010d7e
Ulya Trofimovich <skvadrik@gmail.com>
2021-10-24 13:10:47 +0100

    Small fixes in distcheck script.
    Changes:
      - use 'sh' instead of 'bash' as interpreter
      - split build into stages 1 and 2 and rebuild lexers at stage 2
      - double-quote shell variables to make shellcheck happy

--- build/__distcheck.sh
+++ build/__distcheck.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
@@ -14,22 +14,33 @@ autoreconf -i -W all
 # try to be portable on various MAKEs
 for make_prog in make bmake
 do
-    rm -rf $builddir
-    mkdir $builddir
-    cd $builddir
+    rm -rf "$builddir"
+    mkdir "$builddir"
+    cd "$builddir"
+
+    # stage 1
+    mkdir stage1
+    ../configure --prefix "$(pwd)/stage1" \
+        && $make_prog \
+        && $make_prog install
+
+    # stage 2
     # 'make' implies 'make docs'; running both in parallel may cause data races
     # configure without --enable-debug, this is the release binary
-    ../configure --enable-docs --enable-libs \
-        && $make_prog bootstrap -j$(nproc) \
-        && $make_prog distcheck -j$(nproc)
+    ../configure \
+            --enable-docs \
+            --enable-libs \
+            --enable-lexers RE2C_FOR_BUILD="$(pwd)/stage1/bin/re2c" \
+        && $make_prog bootstrap -j"$(nproc)" \
+        && $make_prog distcheck -j"$(nproc)"
     cd ..
 done
 
 # test that the release tarball builds with CMake
-cd $builddir
+cd "$builddir"
 tarball=$(find . -name 're2c-*.tar.xz')
 test -f "$tarball" && tar -xf "$tarball" \
-    || { echo "*** failed to find tarball ***" && exit 1; }
-cd "${tarball%.tar.xz}" && cmake . && make -j$(nproc) && cd .. \
+    || { echo "*** failed to extract tarball ***" && exit 1; }
+cd "${tarball%.tar.xz}" && cmake . && make -j"$(nproc)" && cd .. \
     || { echo "*** failed to build with cmake ***" && exit 1; }
 cd ..
