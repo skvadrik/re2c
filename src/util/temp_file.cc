skvadrik/re2c

commit 15376f4393d951514c6a8ecd1514b23b751fa698

Define _POSIX_SOURCE for fdopen in cygwin
This fixes the error:
error: ‘fdopen’ was not declared in this scope; did you mean ‘fopen’?

src/util/temp_file.cc
@@ -1,3 +1,7 @@
+#ifndef _POSIX_SOURCE
+#define _POSIX_SOURCE 1 // for fdopen
+#endif
+
 #include "config.h"
 #include <time.h>
 #include "src/util/temp_file.h"

commit f8ea4ea47d9f02373b9171906b65f895e27405a8

Fix the build on FreeBSD.
The build was broken by https://github.com/skvadrik/re2c/pull/376, which
added define of _POSIX_SOURCE to unbreak the build on Cygwin. The build
on FreeBSD is fixed by additionally defining _POSIX_C_SOURCE, which is
required by fdopen.

src/util/temp_file.cc
@@ -1,5 +1,12 @@
+// See https://github.com/skvadrik/re2c/pull/376
+// _POSIX_SOURCE is needed for fdopen on Cygwin.
 #ifndef _POSIX_SOURCE
-#define _POSIX_SOURCE 1 // for fdopen
+#define _POSIX_SOURCE 1
+#endif
+// However the above definition of _POSIX_SOURCE breaks the build on FreeBSD,
+// which also needs _POSIX_C_SOURCE to be defined.
+#ifndef _POSIX_C_SOURCE
+#define _POSIX_C_SOURCE 200112L
 #endif
 
 #include "config.h"