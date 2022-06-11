753c09148f78efd55d49858ff95673edfa782911
Ulya Trofimovich <skvadrik@gmail.com>
2022-06-11 23:09:02 +0100

    Move to C++11: use 'delete' keyword to forbid copying.

--- src/util/forbid_copy.h
+++ src/util/forbid_copy.h
@@ -1,11 +1,8 @@
 #ifndef _RE2C_UTIL_FORBID_COPY_
 #define _RE2C_UTIL_FORBID_COPY_
 
-// must be used at the end of class definition
-// (since this macro changes scope to private)
 #define FORBID_COPY(type) \
-    private: \
-        type (const type &); \
-        type & operator = (const type &)
+    type(const type &) = delete; \
+    type& operator=(const type&) = delete
 
 #endif // _RE2C_UTIL_FORBID_COPY_
