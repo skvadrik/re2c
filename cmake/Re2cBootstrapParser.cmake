6e91c22dfedd5107c6be6aae2af96f30dcad8265
Ulya Trofimovich <skvadrik@gmail.com>
2022-06-23 08:21:37 +0100

    Add CMake option RE2C_REBUILD_PARSERS (it requires Bison).
    Previosuly CMake checked for Bison, and if it was present, then it
    regenerated parsers and updated bootstrap files. Now it checks
    RE2C_REBUILD_PARSERS, and if it is enabled, then CMake fails if it
    cannot find Bison, regenerates parsers and updates bootstrap files.

--- cmake/Re2cBootstrapParser.cmake
+++ cmake/Re2cBootstrapParser.cmake
@@ -1,25 +1,28 @@
 function(re2c_bootstrap_parser ypp_file cc_file h_file)
     get_filename_component(parent_dir "${cc_file}" DIRECTORY)
-    file(RELATIVE_PATH relative_ypp_file "${CMAKE_CURRENT_BINARY_DIR}" "${CMAKE_CURRENT_SOURCE_DIR}/${ypp_file}")
+    file(RELATIVE_PATH relative_parser
+        "${CMAKE_CURRENT_BINARY_DIR}"
+        "${CMAKE_CURRENT_SOURCE_DIR}/${ypp_file}")
 
-    if (BISON_EXECUTABLE)
+    set(bootstrap_parser "${CMAKE_CURRENT_SOURCE_DIR}/bootstrap/${cc_file}")
+    set(bootstrap_header "${CMAKE_CURRENT_SOURCE_DIR}/bootstrap/${h_file}")
+    if (RE2C_REBUILD_PARSERS)
         add_custom_command(
             OUTPUT ${cc_file} ${h_file}
             COMMAND "${CMAKE_COMMAND}" -E make_directory "${parent_dir}"
-            COMMAND "${BISON_EXECUTABLE}" --defines="${h_file}" -o "${cc_file}" "${relative_ypp_file}"
-            COMMAND "${CMAKE_COMMAND}" -E copy_if_different "${cc_file}" "${CMAKE_CURRENT_SOURCE_DIR}/bootstrap/${cc_file}"
-            COMMAND "${CMAKE_COMMAND}" -E copy_if_different "${h_file}" "${CMAKE_CURRENT_SOURCE_DIR}/bootstrap/${h_file}"
+            COMMAND "${BISON_EXECUTABLE}" --defines="${h_file}" -o "${cc_file}" "${relative_parser}"
+            COMMAND "${CMAKE_COMMAND}" -E copy_if_different "${cc_file}" "${bootstrap_parser}"
+            COMMAND "${CMAKE_COMMAND}" -E copy_if_different "${h_file}" "${bootstrap_header}"
             DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/${ypp_file}"
         )
     else()
         add_custom_command(
             OUTPUT ${cc_file} ${h_file}
             COMMAND "${CMAKE_COMMAND}" -E make_directory "${parent_dir}"
-            COMMAND "${CMAKE_COMMAND}" -E copy "${CMAKE_CURRENT_SOURCE_DIR}/bootstrap/${cc_file}" "${parent_dir}"
-            COMMAND "${CMAKE_COMMAND}" -E copy "${CMAKE_CURRENT_SOURCE_DIR}/bootstrap/${h_file}" "${parent_dir}"
-            DEPENDS
-                "${CMAKE_CURRENT_SOURCE_DIR}/bootstrap/${cc_file}"
-                "${CMAKE_CURRENT_SOURCE_DIR}/bootstrap/${h_file}"
+            COMMAND "${CMAKE_COMMAND}" -E copy "${bootstrap_parser}" "${parent_dir}"
+            COMMAND "${CMAKE_COMMAND}" -E copy "${bootstrap_header}" "${parent_dir}"
+            DEPENDS "${bootstrap_parser}" "${bootstrap_header}"
+            COMMENT "Copying bootstrap parser, reconfigure with RE2C_REBUILD_PARSERS to regenerate"
         )
     endif()
 endfunction()
