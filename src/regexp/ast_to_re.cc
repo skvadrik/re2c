a719268ac3dbd2bb1c4745e98290a99abd380f59
Ulya Trofimovich <skvadrik@gmail.com>
2022-07-23 16:54:21 +0100

    Rewrite AST to RE conversion using iteration instead of recursion.
    This partially addresses bug #219.

--- src/regexp/ast_to_re.cc
+++ src/regexp/ast_to_re.cc
@@ -23,8 +23,6 @@
 #include "src/util/check.h"
 #include "src/util/range.h"
 
-namespace re2c {
-
 // note [default regexp]
 //
 // Create a byte range that includes all possible input characters. This may include characters that
@@ -43,11 +41,25 @@ namespace re2c {
 // concatenation (unless it is a capture). Of course, this insertion only applies to subexpressions
 // that have nested captures.
 
+namespace re2c {
+
 struct loc_t;
 
 namespace {
 
-static Ret ast_to_re(RESpec&, const AstNode*, size_t&, int32_t, bool, RE*&) NODISCARD;
+// On-stack information for iterative depth-first conversion of AST to RE.
+struct DfsAstToRe {
+    const AstNode* ast; // current subtree of the AST
+    RE *re1;            // first sub-RE under construction
+    RE *re2;            // second sub-RE under construction (used for alternative and catenation)
+    int32_t height;     // height of the current sub-RE (used in POSIX disambiguation)
+    bool in_iter;       // if this sub-RE is under repetition
+    uint8_t succ;       // index of the current successor node in AST
+
+    DfsAstToRe(const AstNode* ast, int32_t height, bool in_iter)
+            : ast(ast), re1(nullptr), re2(nullptr), height(height), in_iter(in_iter), succ(0) {}
+};
+
 static Ret ast_to_range(RESpec& spec, const AstNode* ast, Range*& r);
 
 LOCAL_NODISCARD(bool has_tags(const AstNode* ast)) {
@@ -74,30 +86,27 @@ LOCAL_NODISCARD(bool has_tags(const AstNode* ast)) {
     return false; // unreachable
 }
 
-static void add_fictive_tags(RESpec& spec, int32_t height, RE** ptag1, RE** ptag2) {
+LOCAL_NODISCARD(RE* fictive_tags(RESpec& spec, int32_t height)) {
     std::vector<Tag>& tags = spec.tags;
 
     // opening fictive tag
-    *ptag1 = re_tag(spec, tags.size(), false);
-    tags.push_back(Tag(Tag::FICTIVE, Tag::FICTIVE, false, false, height + 1));
+    RE* t1 = re_tag(spec, tags.size(), false);
+    tags.emplace_back(Tag::FICTIVE, Tag::FICTIVE, false, false, height + 1);
 
     // closing fictive tag
-    *ptag2 = re_tag(spec, tags.size(), false);
-    tags.push_back(Tag(Tag::FICTIVE, Tag::FICTIVE, false, false, height));
+    RE* t2 = re_tag(spec, tags.size(), false);
+    tags.emplace_back(Tag::FICTIVE, Tag::FICTIVE, false, false, height);
+
+    return re_cat(spec, t1, t2);
 }
 
-static void add_capture_tags(RESpec& spec,
-                             const AstNode** past,
-                             size_t& ncap,
-                             int32_t height,
-                             RE** ptag1,
-                             RE** ptag2,
-                             bool orbit,
-                             bool in_iter) {
+LOCAL_NODISCARD(RE* capture_tags(
+        RESpec& spec, DfsAstToRe& x, bool orbit, const AstNode** psub, size_t* pncap)) {
     std::vector<Tag>& tags = spec.tags;
-    const size_t lcap = ncap;
+    bool history = spec.opts->subhistories && (orbit || x.in_iter);
+    size_t ncap = *pncap, lcap = ncap;
 
-    const AstNode* ast = *past;
+    const AstNode* ast = *psub;
     if (ast->kind == AstKind::CAP) {
         // save the range of repeated captures and collapse them: (...(R)...) -> (R)
         for (ast = ast->cap; ast && ast->kind == AstKind::CAP; ast = ast->cap) {
@@ -107,38 +116,50 @@ static void add_capture_tags(RESpec& spec,
         if (ast->kind == AstKind::REF) {
             ast = ast->ref.ast;
         }
-        *past = ast;
+        *psub = ast;
     }
-
-    bool history = spec.opts->subhistories && (orbit || in_iter);
 
     // opening capture tag
-    *ptag1 = re_tag(spec, tags.size(), false);
-    tags.push_back(Tag(2 * lcap, 2 * ncap, history, orbit, height + 1));
+    RE* t1 = re_tag(spec, tags.size(), false);
+    tags.emplace_back(2 * lcap, 2 * ncap, history, orbit, x.height + 1);
 
     // closing capture tag
-    *ptag2 = re_tag(spec, tags.size(), false);
-    tags.push_back(Tag(2 * lcap + 1, 2 * ncap + 1, history, orbit, height));
+    RE* t2 = re_tag(spec, tags.size(), false);
+    tags.emplace_back(2 * lcap + 1, 2 * ncap + 1, history, orbit, x.height);
 
-    ++ncap;
+    *pncap = ncap + 1;
+    return re_cat(spec, t1, t2);
 }
 
-static inline void add_structural_tags(RESpec& spec,
-                                       const AstNode** past,
-                                       size_t& ncap,
-                                       int32_t height,
-                                       RE** ptag1,
-                                       RE** ptag2,
-                                       bool orbit,
-                                       bool in_iter) {
-    if (spec.opts->autotags) {
+LOCAL_NODISCARD(RE* structural_tags(
+        RESpec& spec, DfsAstToRe& x, const AstNode* sub, size_t* pncap)) {
+    if (sub->kind == AstKind::CAP) {
+        // If this sub-AST is already a capture, no need for structural tags.
+    } else if (spec.opts->autotags) {
         // Full parsing: automatically add tags as if this sub-RE was a capture.
-        add_capture_tags(spec, past, ncap, height, ptag1, ptag2, orbit, in_iter);
-    } else if (spec.opts->posix_semantics && has_tags(*past)) {
+        return capture_tags(spec, x, false, &x.ast, pncap);
+    } else if (spec.opts->posix_semantics && has_tags(x.ast)) {
         // POSIX submatch extraction: add fictive structural tags.
         // See note [POSIX subexpression hierarchy].
-        add_fictive_tags(spec, height, ptag1, ptag2);
+        return fictive_tags(spec, x.height);
     }
+    return nullptr;
+}
+
+// Insert a RE in between a pair of concatenated tags. If there are no tags, just return the RE.
+LOCAL_NODISCARD(RE* insert_between_tags(RESpec& spec, RE* tags, RE* re)) {
+    if (tags == nullptr) {
+        return re;
+    } else if (re != nullptr) {
+        DCHECK(tags->kind == RE::Kind::CAT);
+        DCHECK(tags->cat.re1->kind == RE::Kind::TAG);
+        DCHECK(tags->cat.re2->kind == RE::Kind::TAG);
+        tags->cat.re2 = re_cat(spec, re, tags->cat.re2);
+        if (spec.opts->backward) { // undo the reverse done by concatenation
+            std::swap(tags->cat.re2->cat.re1, tags->cat.re2->cat.re2);
+        }
+    }
+    return tags;
 }
 
 LOCAL_NODISCARD(Ret check_misuse_of_named_def(RESpec& spec, const AstNode* ast)) {
@@ -323,136 +344,162 @@ LOCAL_NODISCARD(Ret re_string(RESpec& spec, const AstNode* ast, RE*& x)) {
     return Ret::OK;
 }
 
-Ret ast_to_re(
-        RESpec& spec, const AstNode* ast, size_t& ncap, int32_t height, bool in_iter, RE*& re) {
+LOCAL_NODISCARD(Ret ast_to_re(RESpec& spec,
+                              std::vector<DfsAstToRe>& stack,
+                              const AstNode* ast0,
+                              size_t* pncap,
+                              RE** presult)) {
     std::vector<Tag>& tags = spec.tags;
     const opt_t* opts = spec.opts;
     Range* r;
+    RE* re;
 
-    if (ast->kind != AstKind::CAP && ast->kind != AstKind::REF) ++height;
+    DCHECK(stack.empty());
+    stack.emplace_back(ast0, 0, false);
 
-    switch (ast->kind) {
-    case AstKind::NIL:
-        re = re_nil(spec);
-        return Ret::OK;
+    while (!stack.empty()) {
+        // Ensure that the reference to stack top won't be accidentally invalidated on push.
+        if (stack.size() == stack.capacity()) stack.reserve(stack.size() * 2);
+        DfsAstToRe& x = stack.back();
 
-    case AstKind::STR:
-        return re_string(spec, ast, re);
+        const AstNode* ast = x.ast;
 
-    case AstKind::CLS:
-        CHECK_RET(cls_to_range(spec, ast, r));
-        return re_class(spec, ast->loc, r, re);
+        if (ast->kind != AstKind::CAP && ast->kind != AstKind::REF) ++x.height;
 
-    case AstKind::DOT:
-        CHECK_RET(dot_to_range(spec, ast, r));
-        return re_class(spec, ast->loc, r, re);
+        switch (ast->kind) {
+        case AstKind::NIL:
+            re = re_nil(spec);
+            stack.pop_back();
+            break;
 
-    case AstKind::DEF:
-        // see note [default regexp]
-        r = spec.rangemgr.ran(0, opts->encoding.nCodeUnits());
-        re = re_sym(spec, r);
-        return Ret::OK;
+        case AstKind::STR:
+            CHECK_RET(re_string(spec, ast, re));
+            stack.pop_back();
+            break;
 
-    case AstKind::DIFF:
-        CHECK_RET(diff_to_range(spec, ast, r));
-        return re_class(spec, ast->loc, r, re);
+        case AstKind::CLS:
+            CHECK_RET(cls_to_range(spec, ast, r));
+            CHECK_RET(re_class(spec, ast->loc, r, re));
+            stack.pop_back();
+            break;
 
-    case AstKind::ALT: {
-        RE* t1 = nullptr, *t2 = nullptr, *t3 = nullptr, *t4 = nullptr, *x, *y;
+        case AstKind::DOT:
+            CHECK_RET(dot_to_range(spec, ast, r));
+            CHECK_RET(re_class(spec, ast->loc, r, re));
+            stack.pop_back();
+            break;
 
-        if (ast->alt.ast1->kind != AstKind::CAP) {
-            add_structural_tags(spec, &ast, ncap, height, &t1, &t2, false, in_iter);
+        case AstKind::DEF: // see note [default regexp]
+            r = spec.rangemgr.ran(0, opts->encoding.nCodeUnits());
+            re = re_sym(spec, r);
+            stack.pop_back();
+            break;
+
+        case AstKind::DIFF:
+            CHECK_RET(diff_to_range(spec, ast, r));
+            CHECK_RET(re_class(spec, ast->loc, r, re));
+            stack.pop_back();
+            break;
+
+        case AstKind::TAG:
+            if (ast->tag.name && !opts->tags) {
+                RET_FAIL(spec.msg.error(
+                        ast->loc, "tags are only allowed with '-T, --tags' option"));
+            } else if (opts->posix_syntax) {
+                RET_FAIL(spec.msg.error(
+                        ast->loc, "simple tags are not allowed with '--posix-captures' option"));
+            }
+            re = re_tag(spec, tags.size(), false);
+            tags.emplace_back(ast->tag.name, ast->tag.history, x.height);
+            stack.pop_back();
+            break;
+
+        case AstKind::CAP:
+            if (!opts->posix_syntax) { // ordinary group, replace with subexpr on stack
+                x.ast = ast->cap;
+            } else { // POSIX capturing group
+                if (x.succ == 0) { // 1st visit: push subexpr
+                    ++x.succ;
+                    x.re1 = capture_tags(spec, x, false, &ast, pncap);
+                    stack.emplace_back(ast,  x.height, x.in_iter);
+                } else { // 2nd visit: return
+                    re = insert_between_tags(spec, x.re1, re);
+                    stack.pop_back();
+                }
+            }
+            break;
+
+        case AstKind::REF:
+            CHECK_RET(check_misuse_of_named_def(spec, ast));
+            x.ast = ast->ref.ast;
+            break;
+
+        case AstKind::ALT:
+            if (x.succ == 0) { // 1st visit: push first subexpr
+                ++x.succ;
+                x.re1 = structural_tags(spec, x, ast->alt.ast1, pncap);
+                stack.emplace_back(ast->alt.ast1, x.height, x.in_iter);
+            } else if (x.succ == 1) { // 2nd visit: push second subexpr
+                ++x.succ;
+                x.re1 = insert_between_tags(spec, x.re1, re);
+                x.re2 = structural_tags(spec, x, ast->alt.ast2, pncap);
+                stack.emplace_back(ast->alt.ast2, x.height, x.in_iter);
+            } else { // 3rd visit: return
+                x.re2 = insert_between_tags(spec, x.re2, re);
+                re = re_alt(spec, x.re1, x.re2);
+                stack.pop_back();
+            }
+            break;
+
+        case AstKind::CAT:
+            if (x.succ == 0) { // 1st visit: push first subexpr
+                ++x.succ;
+                x.re1 = structural_tags(spec, x, ast->cat.ast1, pncap);
+                stack.emplace_back(ast->cat.ast1, x.height, x.in_iter);
+            } else if (x.succ == 1) { // 2nd visit: push second subexpr
+                ++x.succ;
+                x.re1 = insert_between_tags(spec, x.re1, re);
+                x.re2 = structural_tags(spec, x, ast->cat.ast2, pncap);
+                stack.emplace_back(ast->cat.ast2, x.height, x.in_iter);
+            } else { // 3rd visit: return
+                x.re2 = insert_between_tags(spec, x.re2, re);
+                re = re_cat(spec, x.re1, x.re2);
+                stack.pop_back();
+            }
+            break;
+
+        case AstKind::ITER:
+            DCHECK(ast->iter.min <= ast->iter.max);
+            if (x.succ == 0) { // 1st visit: push subexpr
+                ++x.succ;
+                const uint32_t m = ast->iter.max;
+                ast = ast->iter.ast;
+                if ((opts->posix_semantics && ast->kind == AstKind::CAP) || opts->autotags) {
+                    x.re1 = capture_tags(spec, x, m > 1, &ast, pncap);
+                }
+                if (m > 0) {
+                    stack.emplace_back(ast, x.height, true);
+                } else {
+                    re = nullptr;
+                }
+            } else { // 2nd visit: return
+                const uint32_t n = ast->iter.min;
+                const uint32_t m = ast->iter.max;
+                re = insert_between_tags(spec, x.re1, re);
+                if (m > 1) {
+                    re = re_iter(spec, re, std::max(n, 1u), m);
+                }
+                if (n == 0) {
+                    re = re_alt(spec, re, re_nil(spec));
+                }
+                stack.pop_back();
+            }
+            break;
         }
-        CHECK_RET(ast_to_re(spec, ast->alt.ast1, ncap, height, in_iter, x));
-        x = re_cat(spec, t1, re_cat(spec, x, t2));
 
-        if (ast->alt.ast2->kind != AstKind::CAP) {
-            add_structural_tags(spec, &ast, ncap, height, &t3, &t4, false, in_iter);
-        }
-        CHECK_RET(ast_to_re(spec, ast->alt.ast2, ncap, height, in_iter, y));
-        y = re_cat(spec, t3, re_cat(spec, y, t4));
-
-        re = re_alt(spec, x, y);
-        return Ret::OK;
     }
 
-    case AstKind::CAT: {
-        RE* t1 = nullptr, *t2 = nullptr, *t3 = nullptr, *t4 = nullptr, *x, *y;
-
-        if (ast->cat.ast1->kind != AstKind::CAP) {
-            add_structural_tags(spec, &ast, ncap, height, &t1, &t2, false, in_iter);
-        }
-        CHECK_RET(ast_to_re(spec, ast->cat.ast1, ncap, height, in_iter, x));
-        x = re_cat(spec, t1, re_cat(spec, x, t2));
-
-        if (ast->cat.ast2->kind != AstKind::CAP) {
-            add_structural_tags(spec, &ast, ncap, height, &t3, &t4, false, in_iter);
-        }
-        CHECK_RET(ast_to_re(spec, ast->cat.ast2, ncap, height, in_iter,y));
-        y = re_cat(spec, t3, re_cat(spec, y, t4));
-
-        re = re_cat(spec, x, y);
-        return Ret::OK;
-    }
-
-    case AstKind::TAG:
-        if (ast->tag.name && !opts->tags) {
-            RET_FAIL(spec.msg.error(ast->loc, "tags are only allowed with '-T, --tags' option"));
-        }
-        if (opts->posix_syntax) {
-            RET_FAIL(spec.msg.error(ast->loc,
-                                    "simple tags are not allowed with '--posix-captures' option"));
-        }
-        re = re_tag(spec, tags.size(), false);
-        tags.push_back(Tag(ast->tag.name, ast->tag.history, height));
-        return Ret::OK;
-
-    case AstKind::CAP:
-        if (!opts->posix_syntax) {
-            return ast_to_re(spec, ast->cap, ncap, height, in_iter, re);
-        } else {
-            RE* y = nullptr, *t1 = nullptr, *t2 = nullptr;
-            add_capture_tags(spec, &ast, ncap, height, &t1, &t2, false, in_iter);
-            CHECK_RET(ast_to_re(spec, ast, ncap, height, in_iter, y));
-            re = re_cat(spec, t1, re_cat(spec, y, t2));
-        }
-        return Ret::OK;
-
-    case AstKind::REF:
-        CHECK_RET(check_misuse_of_named_def(spec, ast));
-        return ast_to_re(spec, ast->ref.ast, ncap, height, in_iter, re);
-
-    case AstKind::ITER: {
-        const uint32_t n = ast->iter.min;
-        const uint32_t n1 = std::max(n, 1u);
-        const uint32_t m = std::max(n, ast->iter.max);
-        RE* y = nullptr, *t1 = nullptr, *t2 = nullptr;
-
-        ast = ast->iter.ast;
-
-        if ((opts->posix_semantics && ast->kind == AstKind::CAP) || opts->autotags) {
-            add_capture_tags(spec, &ast, ncap, height, &t1, &t2, m > 1, in_iter);
-        }
-
-        if (m == 0) {
-            y = re_cat(spec, t1, t2);
-        } else if (m == 1) {
-            CHECK_RET(ast_to_re(spec, ast, ncap, height, true, y));
-            y = re_cat(spec, t1, re_cat(spec, y, t2));
-        } else  {
-            CHECK_RET(ast_to_re(spec, ast, ncap, height, true, y));
-            y = re_cat(spec, t1, y);
-            y = re_cat(spec, y, t2);
-            y = re_iter(spec, y, n1, m);
-        }
-        if (n == 0) {
-            y = re_alt(spec, y, re_nil(spec));
-        }
-
-        re = y;
-        return Ret::OK;
-    }}
-
+    *presult = re;
     return Ret::OK;
 }
 
@@ -463,10 +510,11 @@ RESpec::RESpec(const opt_t* opts, Msg& msg)
 
 Ret RESpec::init(const std::vector<AstRule>& ast) {
     rules.resize(ast.size());
+    std::vector<DfsAstToRe> stack;
     for (size_t i = 0; i < ast.size(); ++i) {
         size_t ltag = tags.size(), ncap = 0;
         RE* re;
-        CHECK_RET(ast_to_re(*this, ast[i].ast, ncap, 0, false, re));
+        CHECK_RET(ast_to_re(*this, stack, ast[i].ast, &ncap, &re));
         res.push_back(re);
 
         // init rule
