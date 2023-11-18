6f6c0fba7bf77eb27acbaf9eb61a56a5aa128b03
Ulya Trofimovich <skvadrik@gmail.com>
2023-11-18 18:46:36 +0000

    Makefile.am: add a way to embed syntax files into re2c binary and update them.
    Add configure option --enable-syntax.

---
+++ build/stx2cpp.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+"""
+Encodes the contents of a syntax file as C++ source code.
+"""
+
+import sys
+
+if len(sys.argv) != 3:
+    print('usage:', sys.argv[0], '<input> <output>')
+    exit(1)
+
+input = sys.argv[1]
+output = sys.argv[2]
+
+with open(output, 'w') as output_file:
+    output_file.write("const char* DEFAULT_SYNTAX =\n")
+
+    # write input file line by line as a string, escaping characters as needed
+    with open(input, 'r') as input_file:
+        for l in input_file:
+            l_out = l[:-1].translate(str.maketrans({
+                '\\': '\\\\',
+                '"':  '\\"'
+            }))
+            output_file.write('    "' + l_out + '\\n"\n')
+
+    output_file.write("    ;\n")
