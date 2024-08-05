1b5ae59fa5eaadfbccc49b8636bd1213da05bcca
Ulya Trofimovich <skvadrik@gmail.com>
2024-08-05 17:00:25 +0100

    Set `backtick_quoted_strings` configuration for languages that need it.

--- bootstrap/src/default_syntax_js.cc
+++ bootstrap/src/default_syntax_js.cc
@@ -13,7 +13,7 @@ const char* DEFAULT_SYNTAX_JS =
     "\n"
     "semicolons = 0;\n"
     "implicit_bool_conversion = 0;\n"
-    "backtick_quoted_strings = 0;\n"
+    "backtick_quoted_strings = 1;\n"
     "standalone_single_quotes = 0;\n"
     "indentation_sensitive = 0;\n"
     "wrap_blocks_in_braces = 1;\n"
