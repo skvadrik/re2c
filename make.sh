3f3cdafa999b9760f8d320cb703317cae3ab3140
Ulya Trofimovich <skvadrik@google.com>
2024-09-27 06:39:46 +0100

    make.sh: stop on errors.

--- make.sh
+++ make.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+set -e
+
 for lang in {c,go,rust}; do
     python3 build/split_man.py src/manual/manual.rst.in src/manual/manual_$lang.rst $lang
 done
 
-sphinx-build -b html src obj
+sphinx-build -b html src obj || { echo "failed!"; exit 1; }
 
 [ $# != 0 ] || #pass an argument to prevent the server from launching
     python3 -m http.server -d obj
