cd9b9d989aa78f28ae9b3feafd1e4d1a34a86772
Ulya Trofimovich <skvadrik@gmail.com>
2024-10-11 15:12:29 +0100

    Docs (help): use language-specific program name instead of "re2c".
    Split help source file for all language backends.

--- cmake/Re2cGenDocs.cmake
+++ cmake/Re2cGenDocs.cmake
@@ -26,19 +26,24 @@ function(re2c_gen_manpage source target)
     endif()
 endfunction()
 
-function(re2c_gen_help source target bootstrap)
+function(re2c_gen_help source target)
+    file(RELATIVE_PATH relative_target "${CMAKE_CURRENT_BINARY_DIR}" "${target}")
+    set(bootstrap "${CMAKE_CURRENT_SOURCE_DIR}/bootstrap/${relative_target}")
     if(RE2C_REBUILD_DOCS)
         get_filename_component(targetdir "${target}" DIRECTORY)
+        set(split_source "${target}.rst")
         add_custom_command(
             OUTPUT "${target}"
             COMMAND "${CMAKE_COMMAND}" -E make_directory ${targetdir}
-            COMMAND "${PYTHON}" "${re2c_rst2txt}" --variable-name=help "${source}" "${target}"
+            COMMAND "${PYTHON}" "${re2c_splitman}" "${source}" "${split_source}"
+            COMMAND "${PYTHON}" "${re2c_rst2txt}" --variable-name=help "${split_source}" "${target}"
             COMMAND "${CMAKE_COMMAND}" -E copy_if_different "${target}" "${bootstrap}"
             DEPENDS
                 "${source}"
+                "${re2c_splitman}"
                 "${re2c_rst2txt}"
                 "${re2c_docs_sources}"
-            BYPRODUCTS "${target}.1"
+            BYPRODUCTS "${split_source}"
         )
     else()
         add_custom_command(
