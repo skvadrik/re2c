cf302ed2bddbe252127f2a4bab37eff7252a19c9
Ulya Trofimovich <skvadrik@gmail.com>
2024-12-23 16:37:30 +0000

    Add a test for raw newlines in syntax files.

---
+++ test/config/syntax_file_raw_newline.c
@@ -0,0 +1,1 @@
+config/syntax_file_raw_newline.inc:1:22: error: newline in a string literal (use `nl` variable instead)
---
+++ test/config/syntax_file_raw_newline.re
@@ -0,0 +1,1 @@
+// re2c $INPUT -o $OUTPUT --syntax syntax_file_raw_newline.inc
