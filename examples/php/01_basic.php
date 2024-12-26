007da29028ed0dcccd6e72e5aec124a65e9432ae
Anton Vasiliev <1695147@gmail.com>
2024-12-26 22:35:05 +0000

    Regenerate 01_basic.php

--- examples/php/01_basic.php
+++ examples/php/01_basic.php
@@ -4,57 +4,55 @@
 function lex($yyinput) {
     $yycursor = 0;
 
-    {
-        $yych = 0;
-        $yystate = 0;
-        while (true) {
-            switch ($yystate) {
-                case 0:
-                    $yych = $yyinput[$yycursor];
-                    $yycursor += 1;
-                    switch ($yych) {
-                        case 0x31:
-                        case 0x32:
-                        case 0x33:
-                        case 0x34:
-                        case 0x35:
-                        case 0x36:
-                        case 0x37:
-                        case 0x38:
-                        case 0x39:
-                            $yystate = 2;
-                            break;
-                        default:
-                            $yystate = 1;
-                            break;
-                    }
-                case 1:
-                { return false; }
-                case 2:
-                    $yych = $yyinput[$yycursor];
-                    switch ($yych) {
-                        case 0x30:
-                        case 0x31:
-                        case 0x32:
-                        case 0x33:
-                        case 0x34:
-                        case 0x35:
-                        case 0x36:
-                        case 0x37:
-                        case 0x38:
-                        case 0x39:
-                            $yycursor += 1;
-                            $yystate = 2;
-                            break;
-                        default:
-                            $yystate = 3;
-                            break;
-                    }
-                case 3:
-                { return true; }
-                default:
-                    throw new \Exception("internal lexer error");
-            }
+    $yych = 0;
+    $yystate = 0;
+    while (true) {
+        switch ($yystate) {
+            case 0:
+                $yych = $yyinput[$yycursor];
+                $yycursor += 1;
+                switch ($yych) {
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9':
+                        $yystate = 2;
+                        break 2;
+                    default:
+                        $yystate = 1;
+                        break 2;
+                }
+            case 1:
+                return false;
+            case 2:
+                $yych = $yyinput[$yycursor];
+                switch ($yych) {
+                    case '0':
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9':
+                        $yycursor += 1;
+                        $yystate = 2;
+                        break 2;
+                    default:
+                        $yystate = 3;
+                        break 2;
+                }
+            case 3:
+                return true;
+            default:
+                throw new \Exception("internal lexer error");
         }
     }
 
