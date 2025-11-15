skvadrik/re2c

commit 8a4e4ebd5431ba402ab715c17c8ff464a64cdc7a

Unify handling of numeric condition identifiers across code models.
Historically in goto/label mode conditions aren't assigned explicit
numeric identifiers - the numbers are assigned implicitly based on
position in the condition enumeration. However, this correlation is
erroneous: there's no guarantee that the languages that support `goto`
also have enums with implicit numbers.

For now, keep the erroneous correlation (to avoid changing the generated
code), but move it from re2c code to syntax file definitions.

src/codegen/pass2_generate.cc
@@ -1454,11 +1454,7 @@ LOCAL_NODISCARD(Ret expand_tags_directive(Output& output, Code* code)) {
     return Ret::OK;
 }
 
-static void gen_cond_enum(
-        Scratchbuf& buf,
-        OutAllocator& alc,
-        Code* code,
-        const opt_t* opts,
+static void gen_cond_enum(Scratchbuf& buf, OutAllocator& alc, Code* code, const opt_t* opts,
         const StartConds& conds) {
     DCHECK(opts->target == Target::CODE);
 
@@ -1468,19 +1464,16 @@ static void gen_cond_enum(
     if (code->fmt.format) {
         const char* fmt = code->fmt.format;
         const char* sep = code->fmt.separator;
-        uint32_t cond_number = 0;
         for (const StartCond& cond : conds) {
             if (sep != nullptr && &cond != first_cond) {
                 buf.cstr(sep);
             }
             std::ostringstream s(fmt);
-            // The main substitution (the one allowing unnamed sigil) must go last, or else it will
-            // erroneously substitute all the named ones.
-            size_t cid = opts->code_model == CodeModel::GOTO_LABEL ? cond_number : cond.number;
-            argsubst(s, opts->api_sigil, "num", false, cid);
+            // The main substitution (the one allowing unnamed sigil) must go last,
+            // or else it will erroneously substitute all the named ones.
+            argsubst(s, opts->api_sigil, "num", false, cond.number);
             argsubst(s, opts->api_sigil, "cond", true, cond.name);
             buf.str(s.str());
-            ++cond_number;
         }
         buf.cstr("\n");
 
@@ -1491,12 +1484,9 @@ static void gen_cond_enum(
         // prepare an array of enum member names
         const char** ids = alc.alloct<const char*>(conds.size()), **i = ids;
         for (const StartCond& cond : conds) *i++ = buf.str(cond.name).flush();
-        // prepare an array of enum member numbers (only needed in loop/switch or rec/func mode)
-        uint32_t* nums = nullptr;
-        if (opts->code_model != CodeModel::GOTO_LABEL) {
-            uint32_t* j = nums = alc.alloct<uint32_t>(conds.size());
-            for (const StartCond& cond : conds) *j++ = cond.number;
-        }
+        // prepare an array of enum member numbers
+        uint32_t* nums = alc.alloct<uint32_t>(conds.size()), *j = nums;
+        for (const StartCond& cond : conds) *j++ = cond.number;
         // construct enum code item in place of the old code item
         init_code_enum(code, opts->api_cond_type.c_str(), conds.size(), ids, nums);
     }
@@ -1565,6 +1555,17 @@ LOCAL_NODISCARD(Ret expand_cond_enum(Output& output, Code* code)) {
     if (conds.empty()) {
         code->kind = CodeKind::EMPTY;
         return Ret::OK;
+    }
+
+    // In loop/switch or rec/func mode, condition numbers are the numeric indices of their
+    // start DFA state. Otherwise we do not assign explicit numbers, and conditions are
+    // implicitly assigned consecutive numbers starting from zero. Note that conditions from
+    // different blocks may be merged together in this enum, and the enumeration is not per-block
+    // but global across all contributing blocks.
+    if (globopts->code_model == CodeModel::GOTO_LABEL) {
+        for (uint32_t i = 0; i < conds.size(); ++i) {
+            conds[i].number = i;
+        }
     }
 
     gen_cond_enum(buf, alc, code, globopts, conds);

bootstrap/src/default_syntax_c.h
@@ -172,10 +172,11 @@ static constexpr const char* DEFAULT_SYNTAX_C =
     "\n"
     "code:array_elem = array \"[\" index \"]\";\n"
     "\n"
+    "// TODO: generate explicit values in goto/label mode\n"
     "code:enum =\n"
     "    \"enum \" type \" {\" nl indent\n"
-    "        [elem{0:-2}: topindent elem (.init ? \" = \" init) \",\" nl]\n"
-    "        [elem{-1}:   topindent elem (.init ? \" = \" init) nl]\n"
+    "        [elem{0:-2}: topindent elem (!.code_model.goto_label ? \" = \" init) \",\" nl]\n"
+    "        [elem{-1}:   topindent elem (!.code_model.goto_label ? \" = \" init) nl]\n"
     "    dedent \"};\" nl;\n"
     "\n"
     "code:enum_elem = name;\n"

bootstrap/src/default_syntax_d.h
@@ -174,8 +174,8 @@ static constexpr const char* DEFAULT_SYNTAX_D =
     "\n"
     "code:enum =\n"
     "    \"enum \" type \" {\" nl indent\n"
-    "        [elem{0:-2}: topindent elem (.init ? \" = \" init) \",\" nl]\n"
-    "        [elem{-1}:   topindent elem (.init ? \" = \" init) nl]\n"
+    "        [elem{0:-2}: topindent elem (!.code_model.goto_label ? \" = \" init) \",\" nl]\n"
+    "        [elem{-1}:   topindent elem (!.code_model.goto_label ? \" = \" init) nl]\n"
     "    dedent \"};\" nl;\n"
     "\n"
     "code:enum_elem = type \".\" name;\n"

bootstrap/src/default_syntax_go.h
@@ -175,8 +175,8 @@ static constexpr const char* DEFAULT_SYNTAX_GO =
     "\n"
     "code:enum =\n"
     "    \"const (\" nl indent\n"
-    "        [elem{0}:    topindent elem \" = \" (.init ? init : \"iota\") nl]\n"
-    "        [elem{1:-1}: topindent elem (.init ? \" = \" init) nl]\n"
+    "        [elem{0}:    topindent elem \" = \" (!.code_model.goto_label ? init : \"iota\") nl]\n"
+    "        [elem{1:-1}: topindent elem (!.code_model.goto_label ? \" = \" init) nl]\n"
     "    dedent \")\" nl;\n"
     "\n"
     "code:enum_elem = name;\n"

bootstrap/src/default_syntax_v.h
@@ -176,7 +176,7 @@ static constexpr const char* DEFAULT_SYNTAX_V =
     "code:enum = (.code_model.loop_switch\n"
     "    ? [elem: \"const \" elem \" = \" init nl]\n"
     "    : \"enum \" type \" {\" nl\n"
-    "        indent [elem: topindent elem (.init ? \" = \" init) nl] dedent\n"
+    "        indent [elem: topindent elem (!.code_model.goto_label ? \" = \" init) nl] dedent\n"
     "    \"}\" nl);\n"
     "\n"
     "code:enum_elem = (!.code_model.loop_switch ? \".\") name;\n"

include/syntax/c
@@ -171,10 +171,11 @@ code:array_global = code:array_local;
 
 code:array_elem = array "[" index "]";
 
+// TODO: generate explicit values in goto/label mode
 code:enum =
     "enum " type " {" nl indent
-        [elem{0:-2}: topindent elem (.init ? " = " init) "," nl]
-        [elem{-1}:   topindent elem (.init ? " = " init) nl]
+        [elem{0:-2}: topindent elem (!.code_model.goto_label ? " = " init) "," nl]
+        [elem{-1}:   topindent elem (!.code_model.goto_label ? " = " init) nl]
     dedent "};" nl;
 
 code:enum_elem = name;

include/syntax/d
@@ -173,8 +173,8 @@ code:array_elem = array "[" index "]";
 
 code:enum =
     "enum " type " {" nl indent
-        [elem{0:-2}: topindent elem (.init ? " = " init) "," nl]
-        [elem{-1}:   topindent elem (.init ? " = " init) nl]
+        [elem{0:-2}: topindent elem (!.code_model.goto_label ? " = " init) "," nl]
+        [elem{-1}:   topindent elem (!.code_model.goto_label ? " = " init) nl]
     dedent "};" nl;
 
 code:enum_elem = type "." name;

include/syntax/go
@@ -174,8 +174,8 @@ code:array_elem = array "[" index "]";
 
 code:enum =
     "const (" nl indent
-        [elem{0}:    topindent elem " = " (.init ? init : "iota") nl]
-        [elem{1:-1}: topindent elem (.init ? " = " init) nl]
+        [elem{0}:    topindent elem " = " (!.code_model.goto_label ? init : "iota") nl]
+        [elem{1:-1}: topindent elem (!.code_model.goto_label ? " = " init) nl]
     dedent ")" nl;
 
 code:enum_elem = name;

commit deaabdd4cac6b09618ce5a94c12ca65f4fbc7b7f

Fix check for conditions with the same name in goto/label mode.
This check should not compare condition numbers in goto/label mode,
as conditions get renumbered anyway. Previously it used to work because
all numbers were set to zero in goto/label mode, but it was broken by
a recent commit 8a4e4ebd5431ba402ab715c17c8ff464a64cdc7a.

This addresses bug #560 'Illformed C/C++ code due to missing "final"
conditions in generated enumeration'.

src/codegen/pass2_generate.cc
@@ -1493,34 +1493,44 @@ static void gen_cond_enum(Scratchbuf& buf, OutAllocator& alc, Code* code, const 
 }
 
 LOCAL_NODISCARD(Ret add_condition_from_block(
-        const OutputBlock& block, StartConds& conds, StartCond cond)) {
+        const OutputBlock& block, const opt_t* opts, StartConds& conds, StartCond cond)) {
     // Condition prefix is specific to the block that defines it. If a few blocks define conditions
     // with the same name, but a different prefix, they should have different enum entries.
     cond.name = block.opts->cond_enum_prefix + cond.name;
 
-    for (const StartCond& c : conds) {
-        if (c.name == cond.name) {
-            if (c.number == cond.number) {
-                // A duplicate condition, it's not an error but don't add it.
-                return Ret::OK;
-            } else {
-                // An error: conditions with idetical names but different numbers.
-                RET_FAIL(error("cannot generate condition enumeration: condition `%s` has "
-                               "different numbers in different blocks (use `re2c:condenumprefix` "
-                               "configuration to set per-block prefix)",
-                               cond.name.c_str()));
-            }
-        }
+    // In loop/switch or rec/func mode condition numbers are the numeric indices of their start
+    // DFA state. In goto/label mode conditions are renumbered: they are assigned consecutive
+    // numbers starting from zero. Conditions from different blocks are merged together in this
+    // enum, and the enumeration is not per-block but global across all contributing blocks.
+    bool renumber = opts->code_model == CodeModel::GOTO_LABEL;
+
+    auto same = std::find_if(conds.begin(), conds.end(),
+            [=](const StartCond& c){ return c.name == cond.name; });
+
+    if (same == conds.end()) {
+        // Ok, a new condition, add it.
+        if (renumber) cond.number = static_cast<uint32_t>(conds.size());
+        conds.push_back(cond);
+    } else if (same->number == cond.number || renumber) {
+        // Condition with the same name is already on the list: it's fine if the numbers agree,
+        // or if the numbers are reassigned (in goto/label mode). We don't care about `defined`
+        // flags here (they matter for condition dispatch, which is generated on per-block basis).
+    } else {
+        // Error: different conditions with identical names.
+        RET_FAIL(error(
+            "cannot generate condition enumeration: condition `%s` has different numbers in "
+            "different blocks (use `re2c:condenumprefix` configuration to set per-block prefix)",
+            cond.name.c_str()));
     }
 
-    conds.push_back(cond);
     return Ret::OK;
 }
 
-LOCAL_NODISCARD(Ret add_conditions_from_blocks(const blocks_t& blocks, StartConds& conds)) {
+LOCAL_NODISCARD(Ret add_conditions_from_blocks(
+        const blocks_t& blocks, const opt_t* opts, StartConds& conds)) {
     for (const OutputBlock* block : blocks) {
         for (const StartCond& cond : block->conds) {
-            CHECK_RET(add_condition_from_block(*block, conds, cond));
+            CHECK_RET(add_condition_from_block(*block, opts, conds, cond));
         }
     }
     return Ret::OK;
@@ -1542,12 +1552,12 @@ LOCAL_NODISCARD(Ret expand_cond_enum(Output& output, Code* code)) {
     StartConds conds;
     if (code->fmt.block_names == nullptr) {
         // Gather conditions from all blocks in the output and header files.
-        CHECK_RET(add_conditions_from_blocks(output.cblocks, conds));
-        CHECK_RET(add_conditions_from_blocks(output.hblocks, conds));
+        CHECK_RET(add_conditions_from_blocks(output.cblocks, globopts, conds));
+        CHECK_RET(add_conditions_from_blocks(output.hblocks, globopts, conds));
     } else {
         // Gather conditions from the blocks on the list.
         CHECK_RET(find_blocks(output, code->fmt.block_names, output.tmpblocks, "conditions"));
-        CHECK_RET(add_conditions_from_blocks(output.tmpblocks, conds));
+        CHECK_RET(add_conditions_from_blocks(output.tmpblocks, globopts, conds));
     }
 
     // Do not generate empty condition enum. Some compilers or language standards allow it, but
@@ -1555,17 +1565,6 @@ LOCAL_NODISCARD(Ret expand_cond_enum(Output& output, Code* code)) {
     if (conds.empty()) {
         code->kind = CodeKind::EMPTY;
         return Ret::OK;
-    }
-
-    // In loop/switch or rec/func mode, condition numbers are the numeric indices of their
-    // start DFA state. Otherwise we do not assign explicit numbers, and conditions are
-    // implicitly assigned consecutive numbers starting from zero. Note that conditions from
-    // different blocks may be merged together in this enum, and the enumeration is not per-block
-    // but global across all contributing blocks.
-    if (globopts->code_model == CodeModel::GOTO_LABEL) {
-        for (uint32_t i = 0; i < conds.size(); ++i) {
-            conds[i].number = i;
-        }
     }
 
     gen_cond_enum(buf, alc, code, globopts, conds);

test/conditions/undefined_cond_ref.c
@@ -3,21 +3,113 @@
 
 enum YYCONDTYPE {
 	yycx = 0,
-	yycy = 1
+	yycy = 1,
+	yycz = 2,
+	yycw = 3
 };
 
+
+// defined: x, y, z; undefined: w
 
 {
 	YYCTYPE yych;
 	switch (YYGETCONDITION()) {
 		case yycx: goto yyc_x;
-		case yycy: abort();
+		case yycy: goto yyc_y;
+		case yycz: goto yyc_z;
+		case yycw: abort();
 	}
 /* *********************************** */
 yyc_x:
 	if (YYLIMIT <= YYCURSOR) YYFILL(1);
+	yych = *YYCURSOR;
+	switch (yych) {
+		case 'w': goto yy2;
+		case 'y': goto yy3;
+		case 'z': goto yy4;
+		default: goto yy1;
+	}
+yy1:
+	++YYCURSOR;
+	{ * }
+yy2:
+	++YYCURSOR;
+	YYSETCONDITION(yycw);
+	{ x->w }
+yy3:
 	++YYCURSOR;
 	YYSETCONDITION(yycy);
-	{ x -> y }
+	{ x->y }
+yy4:
+	++YYCURSOR;
+	YYSETCONDITION(yycz);
+	{ x->z }
+/* *********************************** */
+yyc_y:
+	if (YYLIMIT <= YYCURSOR) YYFILL(1);
+	yych = *YYCURSOR;
+	switch (yych) {
+		case 'y': goto yy7;
+		default: goto yy6;
+	}
+yy6:
+	++YYCURSOR;
+	{ * }
+yy7:
+	++YYCURSOR;
+	{ y }
+/* *********************************** */
+yyc_z:
+	if (YYLIMIT <= YYCURSOR) YYFILL(1);
+	yych = *YYCURSOR;
+	switch (yych) {
+		case 'z': goto yy10;
+		default: goto yy9;
+	}
+yy9:
+	++YYCURSOR;
+	{ * }
+yy10:
+	++YYCURSOR;
+	{ z }
+}
+
+
+// defined: z, w
+
+{
+	YYCTYPE yych;
+	switch (YYGETCONDITION()) {
+		case yycz: goto yyc_z;
+		case yycw: goto yyc_w;
+	}
+/* *********************************** */
+yyc_z:
+	if (YYLIMIT <= YYCURSOR) YYFILL(1);
+	yych = *YYCURSOR;
+	switch (yych) {
+		case 'z': goto yy14;
+		default: goto yy13;
+	}
+yy13:
+	++YYCURSOR;
+	{ * }
+yy14:
+	++YYCURSOR;
+	{ z }
+/* *********************************** */
+yyc_w:
+	if (YYLIMIT <= YYCURSOR) YYFILL(1);
+	yych = *YYCURSOR;
+	switch (yych) {
+		case 'w': goto yy17;
+		default: goto yy16;
+	}
+yy16:
+	++YYCURSOR;
+	{ * }
+yy17:
+	++YYCURSOR;
+	{ w }
 }
 

test/conditions/undefined_cond_ref.re
@@ -1,6 +1,20 @@
 // re2c $INPUT -o $OUTPUT -ic
 
 /*!conditions:re2c*/
+
+// defined: x, y, z; undefined: w
 /*!re2c
-    <x> * => y { x -> y }
+    <x> [y] => y { x->y }
+    <x> [z] => z { x->z }
+    <x> [w] => w { x->w }
+    <y> [y] { y }
+    <z> [z] { z }
+    <*> * { * }
+*/
+
+// defined: z, w
+/*!re2c
+    <z> [z] { z }
+    <w> [w] { w }
+    <*> * { * }
 */