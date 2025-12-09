5513aff4c008d080acab58336dbaf5ccc732eb6d
Ulya Trofimovich <skvadrik@gmail.com>
2025-12-09 21:55:02 +0000

    Revert "Go backend: add test for signed YYCTYPE."
    This reverts commit b0d740994c0ae8f82f2f833571268b39894176ee.
    
    Reason for revert: don't keep signed YYCTYPE examples for languages
    that don't allow the user to do the incorrect thing (Go, Swift forbid
    comparison of signed 8-bit type to literals that overflow it, like 0xFF;
    Python, JS, OCaml don't even have a signed 8-bit type).

--- test/codegen/go/__run_all.sh
+++ test/codegen/go/__run_all.sh
@@ -2,7 +2,6 @@ root_dir=$(pwd)
 
 for f in $(find -name '*.re'); do
     grep -q 'func main' $f || continue # skip incomplete tests
-    [ "$f" == "./signed_yyctype.re" ] && continue # expected compilation error
 
     echo $f
     cd $(dirname $f)
@@ -11,6 +10,7 @@ for f in $(find -name '*.re'); do
         | egrep -v 'warning: rule .*matches empty string \[-Wmatch-empty-string\]' \
         | egrep -v 'warning: tag .* degree of nondeterminism \[-Wnondeterministic-tags\]' \
         > x.go
+
 
     go run x.go >/dev/null || { echo "*** error ***"; exit 1; }
 
